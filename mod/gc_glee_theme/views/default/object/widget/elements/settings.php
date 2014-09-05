3e5514cd6882036f56948c03e92f30f96b986b0c
Phanoix <phanoix@gmail.com>
2014-09-05 11:36:50 -0400

    Addresses #13, adding a <legend> stating that these are the settings for the widget being edited.

--- mod/gc_glee_theme/views/default/object/widget/elements/settings.php
+++ mod/gc_glee_theme/views/default/object/widget/elements/settings.php
@@ -9,5 +9,5 @@ $widget = elgg_extract('widget', $vars);
 ?>
 
 <div class="elgg-widget-edit" id="widget-edit-<?php echo $widget->guid; ?>">
-	<?php echo elgg_view_form('widgets/save', array(), $vars); ?>
+	<?php echo str_replace( "<fieldset>", "<fieldset><legend><h4>". $widget->getTitle() ." Widget Settings</h4></legend>" , elgg_view_form('widgets/save', array(), $vars) ); ?>
 </div>
