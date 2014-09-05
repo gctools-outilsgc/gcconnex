69c985a7b1c943af75d10bd0df733c9cbf39ff69
Phanoix <phanoix@gmail.com>
2014-09-05 13:59:38 -0400

    Addresses #21.

--- mod/toggle_language/views/default/toggle_language/toggle_lang.php
+++ mod/toggle_language/views/default/toggle_language/toggle_lang.php
@@ -87,16 +87,16 @@
 				//fwrite($log, "session is in english" . "\r\n" );
 
 		?>
-				<span class="active">English</span>
-				<a class="not_active" href="javascript:form_submit('French')">Fran&ccedil;ais</a>
+				<span class="active"><lan=en>English</span>
+				<a class="not_active" href="javascript:form_submit('French')"><lan=fr>Fran&ccedil;ais<lan=en></a>
 
 
 		<?php
 			} else { //if ($SESSION['language'] == 'fr') {
 				//fwrite($log, "session is in french" . "\r\n" );
 		?>
-				<a class="not_active" href="javascript:form_submit('English')">English</a>
-				<span class="active">Fran&ccedil;ais</span>
+				<a class="not_active" href="javascript:form_submit('English')"><lan=en>English</a>
+				<span class="active"><lan=fr>Fran&ccedil;ais</span>
 
 		<?php
 			}
