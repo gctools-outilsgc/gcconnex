14fc7ce9803f1a65b7a02784abb1936f001fad4a
Phanoix <phanoix@gmail.com>
2014-10-29 16:22:31 -0400

    Fix for #20.

--- mod/gc_glee_theme/views/default/glee_theme_draft_one/page/fixed.php
+++ mod/gc_glee_theme/views/default/glee_theme_draft_one/page/fixed.php
@@ -33,7 +33,7 @@ header("Content-type: text/html; charset=UTF-8");
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang=<?php echo get_current_language(); ?> lang=<?php echo get_current_language(); ?>>
 <head>
 
 <?php echo elgg_view('page/elements/head', $vars); ?>
