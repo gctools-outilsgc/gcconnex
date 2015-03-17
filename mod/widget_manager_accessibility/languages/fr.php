1a005c58b5582f4735319820f8d31e2924d0f25f
Phanoix <phanoix@gmail.com>
2015-03-17 14:59:14 -0400

    Added latest version of Phase 2's widget manager accessibility patch mod.

---
+++ mod/widget_manager_accessibility/languages/fr.php
@@ -0,0 +1,38 @@
+<?php 
+
+	$french = array(
+		'widget:unavailable' => 'Supprimer le Widget',
+		'widgets:addButton:title' => "Afficher la fenêtre pour ajouter des widgets",
+
+		// additional widget descriptions
+		"widget-accessibility:info:Activity" => "",
+		"widget-accessibility:info:Blogs"=> "",
+		"widget-accessibility:info:Bookmarks" => "",
+		"widget-accessibility:info:Colleagues" => "",
+		"widget-accessibility:info:Community_Favorites"	=> "",
+		"widget-accessibility:info:Content_by_tag" => "",
+		"widget-accessibility:info:Event_calendar" => "",
+		"widget-accessibility:info:Files" => "",
+		"widget-accessibility:info:Folders"	=> "",
+		"widget-accessibility:info:Free_HTML" => "",
+		"widget-accessibility:info:Group_activity" => "",
+		"widget-accessibility:info:Group_invitations" => "",
+		"widget-accessibility:info:Group_membership" => "",
+		"widget-accessibility:info:Group_News" => "",
+		"widget-accessibility:info:Ideas" => "",
+		"widget-accessibility:info:Latest_community_polls" => "",
+		"widget-accessibility:info:Latest_discussion" => "",
+		"widget-accessibility:info:Latest_poll" => "",
+		"widget-accessibility:info:Messages" => "",
+		"widget-accessibility:info:My_polls" => "",
+		"widget-accessibility:info:Pages" => "",
+		"widget-accessibility:info:RSS_Feed" => "",
+		"widget-accessibility:info:Start_a_discussion" => "",
+		"widget-accessibility:info:Tag_cloud" => "",
+		"widget-accessibility:info:Tasks" => "",
+		"widget-accessibility:info:The_Wire" => "",
+		"widget-accessibility:info:Twitter"	=> "",
+		"widget-accessibility:info:Twitter_search" => "",
+	);
+	add_translation("fr", $french);
+	
---
+++ mod/widget_manager_accessibility/views/default/page/layouts/widgets/add_button.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Button area for showing the add widgets panel
+ */
+
+?>
+<div class="elgg-widget-add-control">
+<?php
+	$options = array(
+			'id' => 'widgets-add-panel',
+			'href' => '#widget_manager_widgets_select',
+			'text' => elgg_echo('widgets:add'),
+			'class' => 'elgg-button elgg-button-action',
+			'is_trusted' => true,
+			'title' => elgg_echo('widgets:addButton:title'),
+			'alt' => elgg_echo('widgets:addButton:title'),
+		);
+	
+	if(elgg_in_context("iframe_dashboard")){
+		$options["style"] = "visibility: hidden;";
+	}
+	echo elgg_view('output/url', $options);
+	
+?>
+</div>
