69f656f35c4c4281a14e84f07ebd38486ddcfa31
Phanoix <phanoix@gmail.com>
2015-03-20 15:37:43 -0400

    Theme css.

--- mod/widget_manager/views/default/widget_manager/css/global.php
+++ mod/widget_manager/views/default/widget_manager/css/global.php
@@ -1,123 +1,157 @@
 <?php
 ?>
+
+
 /* lightbox */
 #widget_manager_widgets_select {
-	margin: 0;
+    margin: 0;
+    width: inherit;
 }
 
 #widget_manager_widgets_select .widget_manager_widgets_lightbox_wrapper {
-	margin-bottom: 5px;
-	border: 1px solid transparent;
+    margin-bottom: 5px;
+    border: 1px solid transparent;
 }
 
 #widget_manager_widgets_select .widget_manager_widgets_lightbox_wrapper:hover {
-	border: 1px solid #CCCCCC;
+    border: 1px solid #CCCCCC;
 }
 
 #widget_manager_widgets_select .widget_manager_widgets_lightbox_actions {
-	float: right;
-	padding: 6px;
+    float: right;
+    padding: 6px;
 }
 
 #widget_manager_widgets_select .widget_manager_widgets_lightbox_actions li {
-	list-style: none;
+    list-style: none;
 }
 
-#widget_manager_widgets_select .widget_manager_widgets_lightbox_actions li.elgg-state-available span,
+#widget_manager_widgets_select .widget_manager_widgets_lightbox_actions li.elgg-state-available span ,
 #widget_manager_widgets_select .widget_manager_widgets_lightbox_actions li.elgg-state-unavailable input {
-	display: none;
+    list-style: none;
+}
+
+#widget_manager_widgets_select .widget_manager_widgets_lightbox_actions li.elgg-state-available input.btn.elgg-button.elgg-button-submit.widget-added {
+    display: none;
+}
+
+#widget_manager_widgets_select .widget_manager_widgets_lightbox_actions li.elgg-state-unavailable input.btn.elgg-button.elgg-button-submit.widget-to-add {
+    display: none;
 }
 
 #widget_manager_widgets_select .widget_manager_widgets_lightbox_actions .submit_button {
-	margin: 5px 0;
+    margin: 5px 0;
 }
 
 #widget_manager_widgets_search {
-	float: right;
+    float: right;
     margin-top: -2px;
 }
 
 #widget_manager_widgets_search input {
-	padding: 0;
-	margin: 0;
-	font-size: 100%;
-	height: 100%;
+    padding: 0;
+    margin: 0;
+    font-size: 100%;
+    height: 100%;
 }
 
 .elgg-module-widget a.widget-manager-widget-title-link {
-	width: auto;
+    width: auto;
 }
 
 .widget_manager_hide_header_admin > .elgg-head {
-	opacity: 0.6;
-	filter: alpha(opacity=60);
+    opacity: 0.6;
+    filter: alpha(opacity=60);
+}
+
+#widget_manager_widgets_select > .elgg-head {
+    position: fixed;
+    width: inherit;
+    z-index: 1;
+    padding: 0px;
+    background: none;
+}
+
+#widget_manager_widgets_select > .elgg-head > h3 {
+    padding: 5px;
+    background: #F1F1F1;
+    margin-right: 15px;
+}
+
+#widget_manager_widgets_select > .elgg-body {
+    background-color: white;
+    padding-top: 40px;
+}
+
+.elgg-module-inline > .elgg-head h3 {
+    color: black;
 }
 
 .elgg-module-widget.widget_manager_disable_widget_content_style > .elgg-body,
 .widget_manager_hide_header > .elgg-body {
-	border-top: 0px;
+    border-top: 0px;
 }
 
 .elgg-module-widget.widget_manager_disable_widget_content_style {
-	background: none;
-	padding: 0px;
+    background: none;
+    padding: 0px;
 }
 
 .elgg-module-widget.widget_manager_disable_widget_content_style .elgg-widget-content {
-	padding: 0px;
+    padding: 0px;
 }
 
 .elgg-module-widget.widget_manager_disable_widget_content_style > .elgg-head {
-	border: 2px solid #DEDEDE;
+    border: 2px solid #DEDEDE;
 }
 
 .elgg-menu-widget .elgg-menu-item-fix {
-	right: 45px;
+    right: 45px;
 }
 
 .elgg-icon-widget-manager-push-pin {
-	background: url("<?php echo elgg_get_site_url(); ?>_graphics/elgg_sprites.png") no-repeat scroll 0px -738px transparent;
+    background: url("<?php echo elgg_get_site_url(); ?>_graphics/elgg_sprites.png") no-repeat scroll 0px -738px transparent;
 }
 
 .widget-manager-fix.fixed > .elgg-icon-widget-manager-push-pin {
-	background-position: 0px -720px;
+    background-position: 0px -720px;
 }
 
 #widget-manager-multi-dashboard {
-	background: #CCC;
+    background: #CCC;
 }
 
 #widget-manager-multi-dashboard-tabs {
-	position: absolute;
-	top: 8px;
+    position: absolute;
+    top: 8px;
 }
 
 .widget-manager-multi-dashboard-tabs-edit {
-	display: none;
-	vertical-align: middle;
+    display: none;
+    vertical-align: middle;
 }
 
 .widget-manager-multi-dashboard-tab:hover > a {
-	padding-right: 5px;
+    padding-right: 5px;
 }
 
 .widget-manager-multi-dashboard-tab:hover .widget-manager-multi-dashboard-tabs-edit {
-	display: inline-block;
-	margin-left: 5px;
+    display: inline-block;
+    margin-left: 5px;
 }
 
 #widget-manager-multi-dashboard-tabs .widget-manager-multi-dashboard-tab-active {
-	border-color: #666666;
+    border-color: #666666;
 }
 #widget-manager-multi-dashboard-tabs .widget-manager-multi-dashboard-tab-hover {
-	border-color: #333333;
+    border-color: #333333;
 }
 
 .widget-manager-groups-widgets-top-row {
-	width: 100%;
-	min-height: 0px !important;
+    width: 100%;
+    min-height: 0px !important;
 }
 
 .widget-manager-groups-widgets-top-row-highlight {
-	min-height: 50px !important;
+    min-height: 50px !important;
 }
