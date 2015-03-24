d1d2c8b3f51e5a162d466dfeed4f98578d5c2c7f
Phanoix <phanoix@gmail.com>
2015-03-24 17:32:41 -0400

    Descriptions now depend on context (profile, dashboard, or group). Description content being filled in now.

--- mod/widget_manager_accessibility/views/default/page/layouts/widgets/add_panel.php
+++ mod/widget_manager_accessibility/views/default/page/layouts/widgets/add_panel.php
@@ -126,7 +126,7 @@ echo elgg_view('input/hidden', $params);
 				
 				$wname = str_ireplace(" ", "_", $widget->name);
 				$body .= "<div><b>" . $widget->name . "</b></div>";
-				$body .= "<div class='elgg-quiet'><img src='" .elgg_get_site_url(). "_graphics/logout.png' alt=" . elgg_echo("widget-accessibility:info:$wname") . " title=". elgg_echo("widget-accessibility:info:$wname") ." /> " . $description . "</div>";
+				$body .= "<div class='elgg-quiet'><img src='" .elgg_get_site_url(). "_graphics/logout.png' alt=\"" . elgg_echo("widget-accessibility:info:$widget_context:$wname") . "\" title=\"". elgg_echo("widget-accessibility:info:$widget_context:$wname") ."\" /> " . $description . "</div>";
 				
 				$body .= "</div>";
 			}
