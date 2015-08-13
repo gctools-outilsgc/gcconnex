f57a4506ce98cad27cd47f6343c9d54921cfa579
Phanoix <phanoix@gmail.com>
2015-08-13 15:12:06 -0400

    update from prod

--- mod/gcRegistration/languages/en.php
+++ mod/gcRegistration/languages/en.php
@@ -19,8 +19,21 @@ $english = array(
 	'gcRegister:mismatch' => 'mismatch',
 
 	// notice
-	'gcRegister:email_notice' => '<b>NOTE:</b> Accounts can only be created using a valid Government of Canada email address. If you entered a non-Government of Canada email address you will receive an <code>invalid email</code> notice. If you believe this notice is in error, please contact: <a href="mailto:gcconnex@tbs-sct.gc.ca">GCconnex@tbs-sct.gc.ca</a>',
-	'gcRegister:terms_and_conditions' => 'I have read, understood, and agree to the <a href="http://gcconnex.gc.ca/terms">terms and conditions of use</a>.',
+	'gcRegister:email_notice' => '
+
+		<strong>NOTE:</strong><br/>
+		Please read before creating an account:
+		<br/><br/>
+
+		1. The system can only create GCconnex accounts using a valid Government of Canada email address ending in gc.ca. If your Government email address is different and you have received an “invalid email notice”, please contact <a href="mailto:GCconnex@tbs-sct.gc.ca">GCconnex@tbs-sct.gc.ca</a>.
+			<br/><br/>
+		2. The system will not let you create a second account with the same email address. If you get an “This email address has already been registered” error, you likely already have a GCconnex account. Please contact <a href="mailto:GCconnex@tbs-sct.gc.ca">GCconnex@tbs-sct.gc.ca</a> to retrieve your GCconnex account credentials.
+			<br/><br/>
+		3. Before you are able to login to GCconnex, you will receive a validation email and you must validate your account. If you do not receive the email, please contact <a href="mailto:GCconnex@tbs-sct.gc.ca">GCconnex@tbs-sct.gc.ca</a>.
+			<br/><br/>
+	',
+	'gcRegister:terms_and_conditions' => 'I have read, understood, and agree to the <a target="_blank" href="http://gcconnex.gc.ca/terms">terms and conditions of use</a>.',
+	'gcRegister:validation_notice' => '<b>NOTE:</b> You will be unable to login to GCconnex until you have received a validation email.',
 	'gcRegister:tutorials_notice' => '<a href="http://www.gcpedia.gc.ca/wiki/Tutorials_on_GC2.0_Tools_/_Tutoriels_sur_les_outils_GC2.0/GCconnex">GCconnex tutorials</a>',
 	
 	// error messages that pop up
--- mod/gcRegistration/languages/fr.php
+++ mod/gcRegistration/languages/fr.php
@@ -1,13 +1,14 @@
 <?php
-$english = array(
+$french = array(
 	// labels
 	'gcRegister:email_initial' => 'Addresse courriel',
 	'gcRegister:email_secondary' => 'Confirmation du courriel',
 	'gcRegister:username' => 'Nom d\'utilisateur (généré automatiquement)',
 	'gcRegister:password_initial' => 'Mot de passe',
 	'gcRegister:password_secondary' => 'Confirmation du mot de passe',
-	'gcRegister:display_name' => 'Nom à afficher<br/>Veuillez écrire votre prénom et votre nom de famille, tel que l’on vous connaît au travail. Conformément aux Conditions d’utilisation, le nom affiché doit correspondre à votre vrai nom. Il n’est pas permis d’utiliser un pseudonyme.',
-	'gcRegister:please_enter_email' => 'Veuillez inscrire votre courriel',
+	'gcRegister:display_name' => 'Nom à afficher',
+	'gcRegister:display_name_notice' => 'Veuillez écrire votre prénom et votre nom de famille, tel que l’on vous connaît au travail. Conformément aux Conditions d’utilisation, le nom affiché doit correspondre à votre vrai nom. Il n’est pas permis d’utiliser un pseudonyme.',
+ 	'gcRegister:please_enter_email' => 'Veuillez inscrire votre courriel',
 	'gcRegister:department_name' => 'Inscrire le nom de votre ministère',
 	'gcRegister:register' => 'S\'inscrire',
 
@@ -18,9 +19,22 @@ $english = array(
 	'gcRegister:mismatch' => 'inadéquation',
 
 	// notice
-	'gcRegister:email_notice' => "<strong>NOTA : </strong>Les comptes ne peuvent être créés qu'avec une adresse courriel valide du gouvernement du Canada. Si vous inscrivez une adresse non valide, vous recevez un avis d'<code>adresse courriel non valide</code>. Si vous croyez que l'adresse courriel du gouvernement du Canada que vous avez utilisée est valide, veuillez communiquer avec <a href='mailto:gcconnex@tbs-sct.gc.ca'>GCconnex@tbs-sct.gc.ca</a>",
-	'gcRegister:terms_and_conditions' => "J'ai lu, je comprends et j'accepte <a href='http://gcconnex.gc.ca/terms'>les conditions d'utilisation</a>.",
-
+	'gcRegister:email_notice' => "
+	
+		<strong>NOTA : </strong><br/>
+		Veuillez lire ce qui suit avant de créer un compte :
+			<br/><br/>
+		1. Une adresse courriel valide du gouvernement du Canada se terminant par gc.ca est requise pour créer un compte GCconnex. Si votre adresse courriel du gouvernement est différente et que vous avez reçu un avis d’adresse courriel non valide, veuillez écrire à <a href='mailto:GCconnex@tbs-sct.gc.ca'>GCconnex@tbs-sct.gc.ca</a>.
+			<br/><br/>
+		2. Le système ne permet pas la création d’un deuxième compte au moyen de la même adresse courriel. Si vous recevez un message vous indiquant que l’adresse courriel est déjà inscrite, vous avez probablement déjà un compte GCconnex. Veuillez écrire à <a href='mailto:GCconnex@tbs-sct.gc.ca'>GCconnex@tbs-sct.gc.ca</a> pour récupérer les données d’identification de votre compte GCconnex.
+			<br/><br/>
+		3. Avant de pouvoir ouvrir une première session dans GCconnex, vous devrez valider votre inscription en cliquant sur un lien de confirmation qui sera envoyé par courriel afin de valider votre compte. Si vous ne recevez pas ce courriel, veuillez écrire à <a href='mailto:GCconnex@tbs-sct.gc.ca'>GCconnex@tbs-sct.gc.ca</a>.
+			<br/><br/>
+	",
+	
+	'gcRegister:terms_and_conditions' => "J'ai lu, je comprends et j'accepte <a target='_blank' href='http://gcconnex.gc.ca/terms'>les conditions d'utilisation</a>.",
+	'gcRegister:tutorials_notice' => '<a href="http://www.gcpedia.gc.ca/wiki/Tutorials_on_GC2.0_Tools_/_Tutoriels_sur_les_outils_GC2.0/GCconnex">Tutoriels sur GCconnex</a>',
+	
 	// error messages that pop up
 	'gcRegister:toc_error' => '- Vous devez accepter les conditions d\'utilisation',
 	'gcRegister:email_in_use' => 'Cette adresse courriel est déjà utilisée',
@@ -30,4 +44,4 @@ $english = array(
 	'gcRegister:display_name_is_empty' => '- Le nom à afficher ne peut être vide',
 );
  
-add_translation("fr", $english);
+add_translation("fr", $french);
