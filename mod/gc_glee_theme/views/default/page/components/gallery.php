4b112e7b0e0c6b492712d9fe987536dda329b88d
Phanoix <phanoix@gmail.com>
2015-08-13 14:58:23 -0400

    Update from prod

--- mod/gc_glee_theme/views/default/page/components/gallery.php
+++ mod/gc_glee_theme/views/default/page/components/gallery.php
@@ -63,14 +63,12 @@ if ($position == 'before' || $position == 'both') {
 				$id = "item-{$item->getType()}-{$item->id}";
 			}
 			echo "<li id=\"$id\" class=\"$item_class\">";
-			
 			if ( $item->getType() == "user" ){
 				$username = get_user( $item->getGUID() )->username;
 				echo str_replace( "</a>", "<div class='hidden wb-invisible'>{$username}</div></a>", elgg_view_list_item($item, $vars) );
 			}
 			else
 				echo elgg_view_list_item($item, $vars);
-			
 			echo "</li>";
 		}
 	?>
