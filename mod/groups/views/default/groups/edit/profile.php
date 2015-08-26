81be0d278c72933c911d588ffa10ad2d81cdf550
Phanoix <phanoix@gmail.com>
2015-08-26 15:15:11 -0400

    Migrated max character limit, count for group editing.

--- mod/groups/views/default/groups/edit/profile.php
+++ mod/groups/views/default/groups/edit/profile.php
@@ -40,40 +40,24 @@ foreach ((array)$group_profile_fields as $shortname => $valtype) {
 
 	$line_break = ($valtype == "longtext") ? "" : "<br />";
 	$label = elgg_echo("groups:{$shortname}");
-	$input = elgg_view("input/{$valtype}", array(
-		"name" => $shortname,
-		"value" => elgg_extract($shortname, $vars),
-	));
 
-	echo "<div><label>{$label}</label>{$line_break}{$input}</div>";
-}
-
-/** TODO: Migrate GCconnex change: character limit, count for Brief discription for Issue #61. 
-$briefmaxlength = 350;					// Maximum length for brief description character count
-if ($group_profile_fields > 0) {
-	foreach ($group_profile_fields as $shortname => $valtype) {
-		$line_break = '<br />';
-		if ($valtype == 'longtext') {
-			$line_break = '';
-		}
-		if ( $shortname == 'briefdescription' )			// Brief description with character limit, count
-			echo '<div><label id="briefdescr-lbl">';
-		else
-			echo '<div><label>';
-		echo elgg_echo("groups:{$shortname}"); if ( $shortname == 'briefdescription' ) echo elgg_echo('groups:brief:charcount') . "0/" . $briefmaxlength;
-		echo "</label>$line_break";
-		if ( $shortname == 'briefdescription' )			// Brief description with character limit, count
-			echo elgg_view("input/{$valtype}", array(
+	if ( $shortname == 'briefdescription' ){				// Brief description with character limit, count
+		$label .= elgg_echo('groups:brief:charcount') . "0/" . $briefmaxlength;	// additional text for max length
+		$input = elgg_view("input/{$valtype}", array(
 			'name' => $shortname,
 			'value' => elgg_extract($shortname, $vars),
 			'maxlength' => $briefmaxlength,
 			'onkeyup' => "document.getElementById('briefdescr-lbl').innerHTML = '" . elgg_echo("groups:{$shortname}") . elgg_echo('groups:brief:charcount') . " ' + this.value.length + '/" . $briefmaxlength . "';"
-			));
-		else
-			echo elgg_view("input/{$valtype}", array(
-				'name' => $shortname,
-				'value' => elgg_extract($shortname, $vars)
-			));
-		echo '</div>';
+		));
 	}
-}**/
+	else
+		$input = elgg_view("input/{$valtype}", array(
+			"name" => $shortname,
+			"value" => elgg_extract($shortname, $vars),
+		));
+
+	if ( $shortname == 'briefdescription' )			// Brief description with character limit, count
+		echo "<div><label id='briefdescr-lbl'>{$label}</label>{$line_break}{$input}</div>";
+	else
+		echo "<div><label>{$label}</label>{$line_break}{$input}</div>";
+}
