3747ccf920f8355a4376b148b11fbbc9af4ea803
Phanoix <phanoix@gmail.com>
2015-09-02 15:55:01 -0400

    widget manager accessiility mod css patch for 1.12 upgrade

--- mod/widget_manager_accessibility/start.php
+++ mod/widget_manager_accessibility/start.php
@@ -10,7 +10,4 @@
 	// register default Elgg events
 	elgg_register_event_handler("init", "system", "widget_manager_accessibility_init");
 
-	// regiser actions
-	elgg_register_action("widget_manager/widgets/toggle_collapse", dirname(__FILE__) . "/actions/widgets/toggle_collapse.php");
-
 	?>
