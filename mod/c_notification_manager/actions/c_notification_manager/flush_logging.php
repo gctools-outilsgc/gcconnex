9f4ce699d4d80b2add7e6520036b5f4a3440c2c6
Christine Yu <internalfire5@live.com>
2015-10-08 08:18:01 -0700

    removed notification manager

--- mod/c_notification_manager/actions/c_notification_manager/flush_logging.php
+++
@@ -1,7 +0,0 @@
-<?php
-//elgg_log('cyu - ' . elgg_get_plugins_path()  . "/c_notification_manager/cronjob_logging.txt", 'NOTICE' );
-$log = fopen(elgg_get_plugins_path()  . "/c_notification_manager/cronjob_logging.txt", 'w');
-fwrite($log, ''. "\r\n" );
-fclose($log);
-
-forward(REFERER);
