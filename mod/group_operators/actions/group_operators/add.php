fb1a3e715e6a39cd0f72ce38fbb37f0b11e6afb3
Phanoix <phanoix@gmail.com>
2015-11-04 10:48:42 -0500

    Fixed group operators functionality, some visual elements still require patching. #113

--- mod/group_operators/actions/group_operators/add.php
+++ mod/group_operators/actions/group_operators/add.php
@@ -5,13 +5,13 @@
  * @package ElggGroupOperators
  */
 
-action_gatekeeper();
+action_gatekeeper("group_operators/add");
 $mygroup = get_entity(get_input('mygroup'));
 $who = get_entity(get_input('who'));
 
 $success = false;
 if ($mygroup instanceof ElggGroup && $who instanceof ElggUser && $mygroup->canEdit()) {
-	if ($mygroup->isMember($who->guid) && !check_entity_relationship($who->guid, 'operator', $mygroup->guid)) {
+	if ($mygroup->isMember($who) && !check_entity_relationship($who->guid, 'operator', $mygroup->guid)) {
 		add_entity_relationship($who->guid, 'operator', $mygroup->guid);
 		system_message(elgg_echo('group_operators:added', array($who->name, $group->name)));
 	} else {
