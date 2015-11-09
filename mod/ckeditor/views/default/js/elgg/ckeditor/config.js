f4c0fb117949c8247679415ec38d263e31550769
Phanoix <phanoix@gmail.com>
2015-11-09 16:43:13 -0500

    Configured ckeditor to include most of the toolbar button + functionality.

--- mod/ckeditor/views/default/js/elgg/ckeditor/config.js
+++ mod/ckeditor/views/default/js/elgg/ckeditor/config.js
@@ -3,7 +3,23 @@ define(function(require) {
 	var $ = require('jquery');
 
 	return {
-		toolbar: [['Bold', 'Italic', 'Underline', 'RemoveFormat'], ['Strike', 'NumberedList', 'BulletedList', 'Undo', 'Redo', 'Link', 'Unlink', 'Image', 'Blockquote', 'Paste', 'PasteFromWord', 'Maximize']],
+		//toolbar: [['Bold', 'Italic', 'Underline', 'RemoveFormat'], ['Strike', 'NumberedList', 'BulletedList', 'Undo', 'Redo', 'Link', 'Unlink', 'Image', 'Blockquote', 'Paste', 'PasteFromWord', 'Maximize']],
+		toolbarGroups: [
+		    //{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		    //{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		    { name: 'links' },
+		    { name: 'insert' },
+		    //{ name: 'forms' },
+		    { name: 'tools' },
+		    { name: 'document',    groups: [ 'mode', 'document', 'doctools' ] },
+		    //{ name: 'others' },
+		    '/',
+		    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		    { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		    { name: 'styles' },
+		    { name: 'colors' },
+		    //{ name: 'about' }
+		    ],
 		removeButtons: 'Subscript,Superscript', // To have Underline back
 		allowedContent: true,
 		baseHref: elgg.config.wwwroot,
