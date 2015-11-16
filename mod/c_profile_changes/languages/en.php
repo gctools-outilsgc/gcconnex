a6aebd5802e1a8bcccbcde77a09345bbb0375fce
Phanoix <phanoix@gmail.com>
2015-11-16 15:51:38 -0500

    Removing merged patch mod, adding to gitignore.

--- mod/c_profile_changes/languages/en.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$english = array(
-    'gcc_profile:error' => 'Did not save: ',
-    'gcc_profile:missingemail' => 'Email address is missing',
-    'gcc_profile:notaccepted' => 'A Government of Canada email address is required'
-);
-
-add_translation("en", $english);
