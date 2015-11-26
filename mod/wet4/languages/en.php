b39dc8e45ed0e12b086feab4b5d63f72c1d065ca
Steph4104 <stephanie4104@gmail.com>
2015-11-26 12:12:12 -0500

    Add toogle to faq section

--- mod/wet4/languages/en.php
+++ mod/wet4/languages/en.php
@@ -1298,7 +1298,38 @@ Please do not reply to this email.",
 *Blogs
 */
     
-'blog:title:all_blogs' => 'Blogs',    
+    'blog:title:all_blogs' => 'Blogs', 
+    
+/**
+*Contact page / FAQ
+*/
+    
+    'contactform:title' => "GCconnex Help Pages",
+    'contactform:list' => "<ul>
+						<li> <a href='http://gcpedia.gc.ca/wiki/Tutorials_on_GC2.0_Tools_/_Tutoriels_sur_les_outils_GC2.0'>How-Tos/Videos/Tutorials</a> </li>
+						<li> <a href='http://gcconnex.gc.ca/file/view/6133688/intro-to-gcconnex-learn-the-how-to-in-6-easy-steps-and-start-using-gcconnex-today'>6 Steps to Using GCconnex</a> </li>
+						<li> <a href='http://gcconnex.gc.ca/file/view/390515/en-managing-your-email-notifications-on-gcconnexpdf'>Managing Email Notifications</a> </li>
+						</ul><br />",
+    'contactform:faq' => "FAQ",
+    'contactform:title:lostpwd' => "Lost Password:",
+    
+    'contactform:content:lostpwd' => "To recover your <b>GCconnex Password</b>, go to the <a href='http://gcconnex.gc.ca/'>GCconnex main page</a> or access the <b>Log in</b> pop-up found in the upper left corner of every GCconnex page. Follow the <b>Lost password</b> link and enter your username or email address, then click <b>Request</b>. A link to reset your password will be sent to email address you associated with your GCconnex account. Follow this link, and then click <b>Reset password</b> to have a new, randomly generated password emailed to your email address. 
+						<br /><br />
+						Once you have logged in with the new password, follow the <b>Settings</b> link found in the upper right corner of every GCconnex page. Under the <b>Account password</b> heading, enter your <b>Current password</b> (the randomly generated password which has been emailed to you) and <b>Your new password</b> (the password you would like to use going forward) twice, then click <b>Save</b>.",
+    
+    'contactform:title:lostuser' => "Lost Username:",
+    
+    'contactform:content:lostuser' => "If you have forgotten your <b>GCconnex Username</b>, don't worry; it is not necessary to recover it. You can use your GCconnex-associated email address to login to GCconnex, either on the <a href='http://gcconnex.gc.ca/'>GCconnex main page</a> or the Log in pop-up found in the upper left hand corner of every GCconnex page. 
+						<br /><br />
+						However, if your email has changed since you signed up for GCconnex, you cannot use the new one to login unless you have <a href='http://gcconnex.gc.ca/settings/'>updated your email in your settings</a>. Please email <a href='mailto:gcconnex@tbs-sct.gc.ca'>gcconnex@tbs-sct.gc.ca</a> and note that you no longer have access to your GCconnex- associated email address. We will get back to you within two business days. ",
+    
+    'contactform:title:create' => "Create Account:",
+    
+    'contactform:content:create' => "Go to <a href='http://gcconnex.gc.ca/'>GCconnex.gc.ca</a> and click \"Register\" (below \"Log in\"). You can then enter your work email address and choose a password. Then read and accept the Terms and Conditions and click \"Register\". ",
+    
+    'contactform:title:picture' => "Upload Profile Picture:",
+    
+    'contactform:content:picture' => "Click on the Profile icon in the top left corner (it will either look like your current profile picture or the default silhouette). You will be directed to a new page that you can then click \"Edit Avatar\". Then click \"Browse\" and select the picture you would like to use and press \"Upload\". You can then crop your picture (if necessary) using the preview section.",
     
 /**
  * Entities
--- mod/wet4/views/default/contactform/contactform.php
+++ mod/wet4/views/default/contactform/contactform.php
@@ -2,8 +2,66 @@
 
 <!-- Form Code Start -->
 <!-- FAQ PART-->
-<?php echo elgg_echo("contactform:body"); ?>
+<?php //echo elgg_echo("contactform:body"); ?>
 <!-- END FAQ PART-->
+
+<h2 style='border-bottom: solid 3px; padding-bottom: 2px;'>Help</h2>
+					<br />
+					<h3><u><?php echo elgg_echo('contactform:title'); ?></u></h3><br />
+						<?php echo elgg_echo('contactform:list'); ?>
+
+					<h3><u><?php echo elgg_echo('contactform:faq'); ?></u></h3><br />
+						<br /><br />
+						<div class="accordion">
+	<!-- Accordion section 1 -->
+	<details class="acc-group">
+		<summary class="wb-toggle tgl-tab" data-toggle='{"parent": ".accordion", "group": ".acc-group"}'><?php echo elgg_echo('contactform:title:lostpwd'); ?></summary>
+		<div class="tgl-panel">
+			<?php echo elgg_echo('contactform:content:lostpwd'); ?>
+		</div>
+	</details>
+	<!-- Accordion section 2 -->
+	<details class="acc-group">
+		<summary class="wb-toggle tgl-tab" data-toggle='{"parent": ".accordion", "group": ".acc-group"}'><?php echo elgg_echo('contactform:title:lostuser'); ?></summary>
+		<div class="tgl-panel">
+		<?php echo elgg_echo('contactform:content:lostuser'); ?>	
+						
+		</div>
+	</details>
+                            <!-- Accordion section 3 -->
+    <details class="acc-group">
+		<summary class="wb-toggle tgl-tab" data-toggle='{"parent": ".accordion", "group": ".acc-group"}'><?php echo elgg_echo('contactform:title:create'); ?></summary>
+		<div class="tgl-panel">
+			<?php echo elgg_echo('contactform:content:create'); ?>
+						
+		</div>
+	</details>
+                            
+                            	<!-- Accordion section 4 -->
+	<details class="acc-group">
+		<summary class="wb-toggle tgl-tab" data-toggle='{"parent": ".accordion", "group": ".acc-group"}'><?php echo elgg_echo('contactform:title:picture'); ?></summary>
+        <div class="tgl-panel">
+		 <?php echo elgg_echo('contactform:content:picture'); ?>
+        </div>
+	</details>
+
+</div>
+				
+						
+
+					<h3><u>GCconnex Groups that may be useful to you:</u></h3><br />
+						<ul>
+						<li> <a href='http://gcconnex.gc.ca/groups/profile/211647/clicks-and-tips'>Clicks and Tips</a> </li>
+						<li> <a href='http://gcconnex.gc.ca/groups/profile/226392/gc20-tools-outils-gc20'>GC2.0 Tools</a> </li>
+						</ul>
+
+				<h2 style='border-bottom: solid 3px; padding-bottom: 2px;'>Contact Us</h2>
+					<p style = 'padding: 8px 0 8px'>
+					Can't find the answer you're looking for in the FAQ or help resources? <br /> <br />
+					<b>Contact the GCconnex <a href='mailto:gcconnex@tbs-sct.gc.ca'>Help Desk</a>!</b> Please be as clear as possible in describing your issue or question, and provide screen shots if and where possible.
+					</p> <br />
+
+						</div>
 
 </body>
 </html>
