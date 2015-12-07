ea9335d2153d5069c0e2d2c3d897a8e1019ad117
Phanoix <phanoix@gmail.com>
2015-12-07 15:11:00 -0500

    Temporary patch to prevent widgets from appearing in edit / settings state by default. #122

--- mod/wet4/views/default/css/elements/modules.php
+++ mod/wet4/views/default/css/elements/modules.php
@@ -23,6 +23,12 @@
 /* ***************************************
 	Widgets
 *************************************** */
-
+.elgg-widget-edit {
+	display: none;
+	/*width: 96%;
+	padding: 2%;
+	border-bottom: 2px solid #dedede;
+	background-color: #f9f9f9;*/
+}
 <?php //@todo location-dependent style: make an extension of elgg-gallery ?>
 
