846ff1ee0264d05f5505c6217f1ecc5c16856f2a
Steph4104 <stephanie4104@gmail.com>
2015-12-18 10:54:35 -0500

    Update group menu

--- mod/wet4/views/default/groups/group_sort_menu.php
+++ mod/wet4/views/default/groups/group_sort_menu.php
@@ -6,16 +6,16 @@
 $user = elgg_get_logged_in_user_entity();
 //create dropdown menu
 
-elgg_register_menu_item('user_menu_tabs', array(
+/*elgg_register_menu_item('user_menu_tabs', array(
     'name' => 'own',
     "href" => "groups/owner/$user->username",
     "text" => elgg_echo("groups:own"),
     'title' => elgg_echo("groups:own"),
     'priority' => '5',
     
-    ));
-
-elgg_register_menu_item('user_menu_tabs', array(
+    ));*/
+if ($user = elgg_get_logged_in_user_entity()) {
+elgg_register_menu_item('tabs_menu', array(
     'name' => 'myGroups',
     "href" => "groups/member/$user->username",
     "text" => elgg_echo("groups:yours"),
@@ -24,7 +24,7 @@ elgg_register_menu_item('user_menu_tabs', array(
     
     ));
 
-elgg_register_menu_item('user_menu_tabs', array(
+elgg_register_menu_item('tabs_menu', array(
     'name' => 'invitations',
     "href" => "groups/invitations/$user->username",
     "text" => elgg_echo("Invitations"),
@@ -32,8 +32,16 @@ elgg_register_menu_item('user_menu_tabs', array(
     'priority' => '6',
     
     ));
-
-$dropdown = elgg_view_menu('user_menu_tabs', array('sort_by' => 'priority','class' => 'dropdown-menu  pull-right'));
+    
+    elgg_register_menu_item('tabs_menu', array(
+    'name' => 'suggested',
+    "href" => "groups/suggested",
+    'text' => elgg_echo('groups:suggested'),
+    'title' => elgg_echo('groups:suggested') . $title,
+    'priority' => '3',
+    ));
+}
+/*$dropdown = elgg_view_menu('user_menu_tabs', array('sort_by' => 'priority','class' => 'dropdown-menu  pull-right'));
 $caret = elgg_echo('<b class="caret"></b>');
 //create tabs menu
 elgg_register_menu_item('tabs_menu', array(
@@ -44,7 +52,7 @@ elgg_register_menu_item('tabs_menu', array(
     'data-toggle' => 'dropdown',
     'class' => 'dropdown-toggle',
     'priority' => '10',
-    ));
+    ));*/
 
 
 
@@ -66,13 +74,7 @@ elgg_register_menu_item('tabs_menu', array(
     'priority' => '2',
     ));
 
-elgg_register_menu_item('tabs_menu', array(
-    'name' => 'suggested',
-    "href" => "groups/suggested",
-    'text' => elgg_echo('groups:suggested'),
-    'title' => elgg_echo('groups:suggested') . $title,
-    'priority' => '3',
-    ));
+
 
 
 echo elgg_view_menu('tabs_menu', array('sort_by' => 'priority', 'id' => 'tabs_menu', 'class' => ' visited-link nav nav-tabs clearfix'));
