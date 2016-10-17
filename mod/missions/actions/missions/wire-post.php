528bfa8e4bfa393057357af509ad2071b2b9f569
piet0024 <n.pietrantonio0024@live.com>
2016-10-17 16:44:22 -0400

    Sharing a mission on the Wire will now function and display as the rest of the site

--- mod/missions/actions/missions/wire-post.php
+++ mod/missions/actions/missions/wire-post.php
@@ -9,7 +9,7 @@
 
 /*
  * Posts a mission notice to The Wire.
- */
+ 
 $message = get_input('wire_message');
 $char_limit = (int)elgg_get_plugin_setting('limit', 'thewire');
 
@@ -23,3 +23,57 @@ else {
 	system_message(elgg_echo('missions:posted_to_the_wire'));
 	forward(elgg_get_site_url() . 'missions/main');
 }
+Nick - Replacing the old share on the wire tools action
+*/
+
+$body = get_input("body", "", false);
+
+$access_id = (int) get_input("access_id", ACCESS_PUBLIC);
+$method = "site";
+$parent_guid = (int) get_input("parent_guid");
+$reshare_guid = (int) get_input("reshare_guid");
+
+// make sure the post isn't blank
+if (empty($body)) {
+	register_error(elgg_echo("thewire:blank"));
+	forward(REFERER);
+}
+
+$guid = thewire_tools_save_post($body, elgg_get_logged_in_user_guid(), $access_id, $parent_guid, $method, $reshare_guid);
+if (!$guid) {
+	register_error(elgg_echo("thewire:error"));
+	forward(REFERER);
+}
+
+// if reply, forward to thread display page
+if ($parent_guid) {
+	$parent = get_entity($parent_guid);
+	forward("thewire/thread/$parent->wire_thread");
+}
+
+// cyu - send notifications when a user shares your content on the wire
+if ($reshare_guid || $reshare_guid > 0) {
+	$content_owner = get_entity($reshare_guid)->getOwnerEntity();
+	$entity = get_entity($reshare_guid);
+	$wire_entity = get_entity($guid);
+
+	if ($entity->getType() == 'group'){
+		$entity->title = $entity->name;
+	}
+
+	// cyu - if cp notification plugin is active, use that for notifications
+	if (elgg_is_active_plugin('cp_notifications')) {
+		$message = array(
+			'cp_msg_type' => 'cp_wire_share',
+			'cp_recipient' => $entity->getOwnerEntity(),
+			'cp_shared_by' => elgg_get_logged_in_user_entity(),
+			'cp_content' => $entity,
+			'cp_wire_url' => $wire_entity->getURL(),
+		);
+		elgg_trigger_plugin_hook('cp_overwrite_notification','all',$message);
+	}
+}
+
+//Take them back to the missions screen
+system_message(elgg_echo('missions:posted_to_the_wire'));
+forward(elgg_get_site_url() . 'missions/main');
