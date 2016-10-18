0b34299a682c786dd44461d8bb639d369559164f
ethanWallace <ethan.wallace.91@gmail.com>
2016-10-18 11:15:10 -0400

    Admin menu display
    Badge should be normal size

---
+++ mod/gcProfilePictureBadges/views/default/widgets/avatar_style.php
@@ -0,0 +1,12 @@
+<style>
+.gcInitBadge {
+        position: absolute;
+         margin-left: auto;
+         margin-right: auto;
+         left:0;
+         right:0;
+         width:80%;
+         bottom:-10%;
+
+}
+</style>
