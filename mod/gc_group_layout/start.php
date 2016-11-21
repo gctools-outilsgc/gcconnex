3e6c3356c1da613e7e66d2b9374be36f7a476653
piet0024 <n.pietrantonio0024@live.com>
2016-11-21 16:22:23 -0500

    Create base mod

---
+++ mod/gc_group_layout/start.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+* GC Group Layout - modifies the group layout
+*
+* @version 1.0
+* @author Nick
+*/
+
+elgg_register_event_handler('init','system','gc_group_layout_init');
+
+function gc_group_layout_init(){
+  
+}
