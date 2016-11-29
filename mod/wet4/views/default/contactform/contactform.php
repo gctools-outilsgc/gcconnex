2655435f9c5b3bd7da48939b4738b1f114b6fe49
piet0024 <n.pietrantonio0024@live.com>
2016-11-29 11:31:28 -0500

    Comments on blog + contactform

--- mod/wet4/views/default/contactform/contactform.php
+++ mod/wet4/views/default/contactform/contactform.php
@@ -2,7 +2,16 @@
 
 <!-- Form Code Start -->
 <!-- FAQ PART-->
-<?php //echo elgg_echo("contactform:body"); ?>
+<?php //echo elgg_echo("contactform:body"); 
+/*
+ * contactform.php
+ * 
+ * Handles the layout for contact form. Creates a FAQ list for the site as well.
+ * 
+ * @package wet4
+ * @author GCTools Team
+ */
+?>
 <!-- END FAQ PART-->
 
 <h2 class="h2"><?php echo elgg_echo('contactform:help'); ?></h2>
