20f5ed0b7986fc133cf8bd1add064037d7e83d64
Phanoix <phanoix@gmail.com>
2016-11-30 16:14:09 -0500

    Same as last commit with changes

--- mod/file/pages/file/download.php
+++ mod/file/pages/file/download.php
@@ -29,7 +29,7 @@ header("Content-type: $mime");
 if (strpos($mime, "image/") !== false || $mime == "application/pdf") {
 	header("Content-Disposition: inline; filename=\"$filename\"");
 } else {
-	header("Content-Disposition: inline; filename=\"$filename\"");
+	header("Content-Disposition: attachment; filename=\"$filename\"");
 }
 header("Content-Length: {$file->getSize()}");
 
