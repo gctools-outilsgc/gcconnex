3e1e122cf1d31196ac0c81c30845063af5e3d1c3
piet0024 <n.pietrantonio0024@live.com>
2016-12-02 14:21:45 -0500

    Recreating old css views used to overwrite core views (#556)
    Adding these files back to fix layout issues caused by wet clean up.

---
+++ mod/wet4/views/default/css/elements/components.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Layout Object CSS
+ *
+ * Image blocks, lists, tables, gallery, messages
+ *
+ * @package Elgg.Core
+ * @subpackage UI
+ *
+ * GC_MODIFICATION
+ * Description: This file exists to overwrite the core image block and component styles
+ * Author: Nick github.com/piet0024
+ */
+?>
+/* <style> /**/
+/* ***************************************
+	Image Block
+*************************************** */
+/******* Custom CSS for messages *********/
