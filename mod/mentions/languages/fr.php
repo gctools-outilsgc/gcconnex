b35850330ff8b8d84344831b1f152c50793576b0
ethanWallace <ethan.wallace.91@gmail.com>
2016-12-07 09:17:29 -0500

    Mentions cleanup (#562)
    Added gc_modifcation header to files we modified. Also fixed Frech
    language file having display issues with french characters.

--- mod/mentions/languages/fr.php
+++ mod/mentions/languages/fr.php
@@ -4,19 +4,19 @@
  */
 
 return array(
-	'mentions:notification:subject' => '%s vous a cité dans %s',
-	'mentions:notification:body' => '%s vous a cité dans %s.
+	'mentions:notification:subject' => '%s vous a citÃ© dans %s',
+	'mentions:notification:body' => '%s vous a citÃ© dans %s.
 
-Pour voir la publication entière, cliquez sur le lien ci-dessous:
+Pour voir la publication entiÃ¨re, cliquez sur le lien ci-dessous:
 %s
 ',
 	'mentions:notification_types:object:blog' => 'un billet de blogue',
 	'mentions:notification_types:object:bookmarks' => 'un signet',
 	'mentions:notification_types:object:groupforumtopic' => 'une publication dans un groupe de discussion',
-	'mentions:notification_types:object:discussion_reply' => 'une réponse dans un groupe de discussion',
+	'mentions:notification_types:object:discussion_reply' => 'une rÃ©ponse dans un groupe de discussion',
 	'mentions:notification_types:object:thewire' => 'un message sur le fil',
 	'mentions:notification_types:object:comment' => 'un commentaire',
-	'mentions:settings:send_notification' => 'Envoyer une notification lorsqu’une personne vous @cite dans une publication?',
+	'mentions:settings:send_notification' => 'Envoyer une notification lorsquï¿½une personne vous @cite dans une publication?',
 
 	// admin
 	'mentions:fancy_links' => 'Replce @mentions with a small picture of the user in addition to the user\'s name',
