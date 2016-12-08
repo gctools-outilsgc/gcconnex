adce15a259eb35e3462bd4c5858cff8a5325f114
Christine <sokguan@gmail.com>
2016-12-08 15:24:01 -0800

    resolved conflict

--- mod/cp_notifications/start.php
+++ mod/cp_notifications/start.php
@@ -1056,16 +1056,16 @@ function cp_create_notification($event, $type, $object) {
 				$guid_two = $object->getOwnerGUID();
 
 					if (!$object->title) {
-						$subject = elgg_echo('cp_notify_usr:subject:new_content2',array($object->getOwnerEntity()->username,$object->getSubtype()),'en');
-						$subject .= ' | '.elgg_echo('cp_notify_usr:subject:new_content2',array($object->getOwnerEntity()->username,$object->getSubtype()),'fr');
+						$subject = elgg_echo('cp_notify_usr:subject:new_content2',array($object->getOwnerEntity()->username,cp_translate_subtype($object->getSubtype())),'en');
+						$subject .= ' | '.elgg_echo('cp_notify_usr:subject:new_content2',array($object->getOwnerEntity()->username,cp_translate_subtype($object->getSubtype(), false)),'fr');
 					} else {
 
 						if (strcmp($object->getSubtype(), 'hjforumpost') != 0 || strcmp($object->getSubtype(), 'hjforumtopic') != 0) {
 							if($object->title1) {
 								$object->title = $object->title1;
 							}
-							$subject = elgg_echo('cp_notify_usr:subject:new_content',array($object->getOwnerEntity()->username,$object->getSubtype(),$object->title),'en');
-							$subject .= ' | '.elgg_echo('cp_notify_usr:subject:new_content',array($object->getOwnerEntity()->username,$object->getSubtype(),$object->title),'fr');
+							$subject = elgg_echo('cp_notify_usr:subject:new_content',array($object->getOwnerEntity()->username,cp_translate_subtype($object->getSubtype()),$object->title),'en');
+							$subject .= ' | '.elgg_echo('cp_notify_usr:subject:new_content',array($object->getOwnerEntity()->username,cp_translate_subtype($object->getSubtype(), false),$object->title),'fr');
 						}
 					}
 				}
@@ -1084,7 +1084,7 @@ function cp_create_notification($event, $type, $object) {
 	} // end of switch statement
 
 
-	// check for empty subjects or empty content
+	// check for empty subjects or empty content 
 	if (empty($subject))
 		return false;
 
@@ -1370,19 +1370,19 @@ function cp_translate_subtype($subtype_name, $english = true) {
 	$label = '';
 	switch($subtype_name) {
 		case 'blog':
-				$label = 'blog';
+			$label = 'blog';
 			break;
 		case 'bookmarks':
-				$label = 'bookmark';
+			$label = 'bookmark';
 			break;
 		case 'file':
-				$label = 'file';
+			$label = 'file';
 			break;
 		case 'poll':
-				$label = 'poll';
+			$label = 'poll';
 			break;
 		case 'event_calendar':
-				$label = 'event';
+			$label = 'event';
 			break;
 		case 'album':
 			$label = 'album';
@@ -1408,6 +1408,8 @@ function cp_translate_subtype($subtype_name, $english = true) {
 			break;
 		case 'thewire':
 			$label = 'wire';
+			if (!$english)
+				$label = 'fil';
 			break;
 		case 'task_top':
 			$label = 'task';
