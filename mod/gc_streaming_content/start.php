c351b191f284460f3813c3c8e6089d47e641232f
piet0024 <n.pietrantonio0024@live.com>
2016-12-13 10:28:38 -0500

    Creating Base mod

---
+++ mod/gc_streaming_content/start.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+* start.php
+*
+* 
+*
+* @author Nick github.com/piet0024
+*/
+
+elgg_register_event_handler('init', 'system', 'gc_streaming_init');
+
+function gc_streaming_init(){
+    
+    
+}
---
+++ mod/gc_streaming_content/views/default/js/stream_wire.js
@@ -0,0 +1,7 @@
+/*
+* stream_wire.js
+*
+* Streaming Wire JS - Does things
+*
+* @author Nick github.com/piet0024
+*/
