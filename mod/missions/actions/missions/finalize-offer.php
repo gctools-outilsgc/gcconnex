b2d3d1e6f20b7e04c7047938aa381a5e43c08b51
Phanoix <phanoix@gmail.com>
2017-01-16 16:12:23 -0500

    swapped sender and destination

--- mod/missions/actions/missions/finalize-offer.php
+++ mod/missions/actions/missions/finalize-offer.php
@@ -50,7 +50,7 @@ else {
 
 			$subject = elgg_echo('missions:participating_in', array(elgg_get_excerpt($mission->job_title, elgg_get_plugin_setting('mission_job_title_card_cutoff', 'missions'))));
 			$body = elgg_echo('missions:participating_in_more', array($applicant->name)) . $mission_link . '.';
-			mm_notify_user($mission->guid, $applicant->guid, $subject, $body);
+			mm_notify_user($applicant->guid, $mission->guid, $subject, $body);
 		}
 	}
 }
