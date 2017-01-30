21272cc8ae1bdc8b11facdd375bc77f86ac26df7
pandurx <internalfire5@live.com>
2017-01-30 08:48:00 -0500

    update

--- mod/gc_fedsearch_gsa/start.php
+++ mod/gc_fedsearch_gsa/start.php
@@ -28,7 +28,7 @@ function entity_title($hook, $type, $return, $params) {
 			return;
 
 		$url = explode('/',$_SERVER['REQUEST_URI']);
-		$entity = get_entity($url[3]);
+		$entity = get_entity($url[4]);
 
 		// let the gsa index the title of the entity as well
 		foreach ($entity_title->getElementsByTagName('h1')->item(0)->childNodes as $node) 
