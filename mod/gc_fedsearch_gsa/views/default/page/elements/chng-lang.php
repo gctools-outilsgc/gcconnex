1883920acaccc486edee96c5358715c6fcdabf6b
pandurx <internalfire5@live.com>
2017-02-21 17:50:11 -0800

    condition revised

--- mod/gc_fedsearch_gsa/views/default/page/elements/chng-lang.php
+++ mod/gc_fedsearch_gsa/views/default/page/elements/chng-lang.php
@@ -13,6 +13,7 @@ $site_url = elgg_get_site_url();
 
 if (elgg_is_active_plugin('gc_fedsearch_gsa') && ((!$gsa_usertest) && strcmp($gsa_agentstring,strtolower($_SERVER['HTTP_USER_AGENT'])) == 0) || strstr(strtolower($_SERVER['HTTP_USER_AGENT']), 'gsa-crawler') !== false ) {
   
+  } else {
 ?>
     <section id="wb-lng" class="visible-md visible-lg text-right">
             <h2 class="wb-inv"><?php echo elgg_echo('wet:LangSel')?></h2>
