d8eca40ef5f5198e7575cbda87b2f3c7dbcb30ee
Phanoix <phanoix@gmail.com>
2017-03-07 13:58:12 -0500

    lang variables

--- mod/gc_streaming_content/languages/fr.php
+++ mod/gc_streaming_content/languages/fr.php
@@ -2,4 +2,6 @@
 
 return array(
     'stream:new_wire'=>"Il y a de nouveaux messages. Cliquez ici pour",
+    'stream:new_newfeed'=>"Il y a de nouveaux messages de fil de nouvelles. Cliquez ici pour",
+	'custom_index_widgets:stream_newsfeed_index' => "Stream Newsfeed items (Translate Me Please!)",
 );
