9daad6ae8bdda127adaca3bae44d548f5269f900
piet0024 <n.pietrantonio0024@live.com>
2017-03-23 13:14:56 -0400

    River accessibility changes (#868)
    * Image blocks are now in article tags / avatars for newsfeed screen reader hidden
    
    * adding nav landmark to page filter menus

--- mod/wet4/views/default/page/components/image_block.php
+++ mod/wet4/views/default/page/components/image_block.php
@@ -142,7 +142,7 @@ HTML;
     $body = "<div class=\"mrgn-tp-sm col-xs-10 noWrap\">$body</div>";
 
 if ($image) {
-	$image = "<div class=\"mrgn-tp-sm col-xs-2\">$image</div>";
+	$image = "<div aria-hidden=\"true\" class=\"mrgn-tp-sm col-xs-2\">$image</div>";
      //$echo = elgg_get_context();
 }
 
@@ -153,13 +153,13 @@ if ($alt_image) {
 
 echo <<<HTML
 
-<div class="$class clearfix mrgn-bttm-sm" $id>
+<article class="$class clearfix mrgn-bttm-sm" $id>
 
 	$image$alt_image$body$echo
     <div class=" elgg-body clearfix edit-comment">
    
     </div>
-</div>
+</article>
 HTML;
 
 }
