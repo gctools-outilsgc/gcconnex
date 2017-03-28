06a2ba0e8a73280b2af29d089faaeda0a49a84d1
piet0024 <n.pietrantonio0024@live.com>
2017-03-28 15:53:36 -0400

    Event calendar objects now close, this was causing layout issues on profile

--- mod/b_extended_profile/views/default/profile/tab-content.php
+++ mod/b_extended_profile/views/default/profile/tab-content.php
@@ -131,7 +131,6 @@ echo '<div role="tabpanel" tabindex="-1" class="tab-pane fade-in" id="events">';
 
     foreach($events as $event) {
 		echo elgg_view("object/event_calendar", array('entity' => $event));
-        echo '</div>';
 	}
 
     $date = date('Y-m-d'/*, strtotime("-1 days")*/);
