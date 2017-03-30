a27c7085df4e7b3f871fab6fc872d2bef3f78edd
LemieuxGen <genevieve.lemieux@tbs-sct.gc.ca>
2017-03-30 12:00:38 -0400

    Update en.php
    New changes - minor. they are marked with "CHANGE 2"

--- mod/cp_notifications/languages/en.php
+++ mod/cp_notifications/languages/en.php
@@ -62,7 +62,7 @@ $english = array(
 	'cp_notifications:mail_body:subtype:photo' => "%s posted an image: %s", //CHANGE 1
 	'cp_notifications:mail_body:subtype:task' => "%s created a task: %s", //CHANGE 1
 	'cp_notifications:mail_body:subtype:likes' => "%s liked your post: %s",
-	'cp_notifications:mail_body:subtype:response' => "%s posted a response or comment to your post: %s",
+	'cp_notifications:mail_body:subtype:response' => "%s replied or commented on your post: %s", // CHANGE 2
 	'cp_notifications:mail_body:subtype:any' => "%s posted a%s %s: %s", // john doe posted an idea vs john doe posted a blog
 
 	'cp_notifications:mail_body:subtype:content_revision' => "%s revised the %s: %s", //CHANGE 1
