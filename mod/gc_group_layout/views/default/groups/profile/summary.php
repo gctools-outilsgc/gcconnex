84e37e6cdeb795e80b684d38d09c138aca9dd442
piet0024 <n.pietrantonio0024@live.com>
2017-03-30 12:50:55 -0400

    clean up file

--- mod/gc_group_layout/views/default/groups/profile/summary.php
+++ mod/gc_group_layout/views/default/groups/profile/summary.php
@@ -294,14 +294,11 @@ if($group->cover_photo =='nope' || $group->cover_photo ==''){
                         //Nick - Added a link to share the group on the wire
                         if(elgg_is_logged_in()){
                             $options = array(
-
                                 'text' => '<i class="fa fa-share-alt fa-lg icon-unsel"><span class="wb-inv">Share this group on the Wire</span></i>',
                                 'title' => elgg_echo('thewire_tools:reshare'),
                                 'href' => 'ajax/view/thewire_tools/reshare?reshare_guid=' . $group->getGUID(),
                                 'class' => 'elgg-lightbox',
-
                                 'is_trusted' => true,
-
                             );
                         }
                         echo '<div class="pull-left mrgn-rght-sm">'.elgg_view('output/url', $options).'</div>';
@@ -309,25 +306,20 @@ if($group->cover_photo =='nope' || $group->cover_photo ==''){
                         //Nick - Added a link to like the group!
                         if(elgg_is_logged_in()){
                             $hasLiked = \Elgg\Likes\DataService::instance()->currentUserLikesEntity($group->guid);
-
 		                      //Has this user liked this already?
                             if($hasLiked){
                                 $options = array(
-			                     'name' => 'unlike',
 			                     'href' => elgg_add_action_tokens_to_url("/action/likes/delete?guid={$group->guid}"),
 			                     'text' => '<i class="fa fa-thumbs-up fa-lg icon-sel"></i><span class="wb-inv">'. elgg_echo('likes:remove').'</span>',
 			                     'title' => elgg_echo('likes:remove') . ' ' .elgg_echo('group'),
-			                     'item_class' => $hasLiked ? 'pad-rght-xs' : 'hidden',
-			                     'priority' => 998,
+			                     
 		                      );
                             }else{
                                $options = array(
-			                     'name' => 'likes',
 			                     'href' => elgg_add_action_tokens_to_url("/action/likes/add?guid={$group->guid}"),
 			                     'text' => '<i class="fa fa-thumbs-up fa-lg icon-unsel"></i><span class="wb-inv">'.elgg_echo('likes:likethis').'</span>',
                                  'title' => elgg_echo('likes:likethis') . ' ' . elgg_echo('group'),
-			                     'item_class' => $hasLiked ? 'hidden' : '',
-			                     'priority' => 998,
+			                     
 		                      ); 
                             }
 
