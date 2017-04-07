23ec35b593752cb266d05f3ed6e77f92adc4e7a9
Adrien Pyke <adpyke@gmail.com>
2017-04-07 11:09:05 -0400

    added infinite scroll to other wire views

--- mod/gc_streaming_content/views/default/js/infinite_wire.js
+++ mod/gc_streaming_content/views/default/js/infinite_wire.js
@@ -8,11 +8,18 @@
 	'use strict';
 
 	var SCRIPT_NAME = 'infinite_wire';
-	var BASE_SITE = elgg.normalize_url();
-	var WIRE_URL = BASE_SITE + 'thewire/all';
 	var API = 'ajax/view/ajax/wire_posts';
+	var PATHNAME = window.location.pathname;
+	var ALL_REGEX = /^\/thewire\/all/;
+	var FRIENDS_REGEX = /^\/thewire\/friends\/([^\/]+)/;
+	var OWNER_REGEX = /^\/thewire\/owner\/([^\/]+)/;
+	var SEARCH_REGEX = /^\/thewire\/search\/?([^\/]+)?/;
 
-	if (window.location.href === WIRE_URL) {
+	// If they're on one of the wire pages/^\/thewire\/search\/?(.+)?/
+	if (PATHNAME.match(ALL_REGEX) ||
+		PATHNAME.match(FRIENDS_REGEX) ||
+		PATHNAME.match(OWNER_REGEX) ||
+		PATHNAME.match(SEARCH_REGEX)) {
 		var Util = {
 			log: function() {
 				var args = [].slice.call(arguments);
@@ -24,61 +31,121 @@
 			},
 			qq: function(query, context) {
 				return [].slice.call((context || document).querySelectorAll(query));
+			},
+			getQueryParameter: function(name, url) {
+				if (!url) url = window.location.href;
+				name = name.replace(/[\[\]]/g, "\\$&");
+				var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+					results = regex.exec(url);
+				if (!results) return null;
+				if (!results[2]) return '';
+				return decodeURIComponent(results[2].replace(/\+/g, " "));
 			}
 		};
 
-		var wireContent = Util.q('#wb-cont > ul.elgg-list');
-
-		var extractId = function(elem) {
-			var regex = /^elgg\-object\-(\d+)$/;
-			return parseInt(elem.id.match(regex)[1]);
+		var Posts = {
+			postsElement: Util.q('#wb-cont > ul.elgg-list'),
+			// extracts the GUID from the id of the posts element
+			extractId: function(elem) {
+				var regex = /^elgg\-object\-(\d+)$/;
+				return parseInt(elem.id.match(regex)[1]);
+			},
+			// Returns an array of the ids of all loaded posts
+			getCurrent: function() {
+				return Util.qq('.elgg-item-object-thewire', Posts.postsElement).map(function(elem) {
+					return Posts.extractId(elem);
+				});
+			}
 		};
 
-		// Returns an array of the ids of all loaded posts
-		var getCurrentPosts = function() {
-			return Util.qq('.elgg-item-object-thewire', wireContent).map(function(elem) {
-				return extractId(elem);
-			});
-		};
+		var runScript = true;
 
-		var isLoading = false;
-		window.addEventListener('scroll', function(e) {
-			if (!isLoading) {
-				// when we've scrolled past the bottom of the wire content load the stuff
-				if (wireContent.getBoundingClientRect().bottom <= window.innerHeight) {
-					isLoading = true;
-					elgg.get(API, {
-						data: {
-							'limit': 15,
-							'offset': getCurrentPosts().length
-						},
-						dataType: 'html',
-						success: function (data) {
-							var tempDiv = document.createElement('div');
-							tempDiv.innerHTML = data;
+		var friendsOf;
+		var username;
+		var query;
 
-							var currentPosts = getCurrentPosts();
-							var oldestPost;
-							if (currentPosts.length > 0) {
-								oldestPost = Math.min.apply(null, currentPosts);
-							}
+		var match = PATHNAME.match(FRIENDS_REGEX);
+		if (match) {
+			friendsOf = match[1];
+		}
 
-							Util.qq('.elgg-item-object-thewire', tempDiv).filter(function(elem) {
-								// Only include posts that are older than the oldest post
-								if (oldestPost) {
-									var id = extractId(elem);
-									return id < oldestPost;
-								}
-								return true;
-							}).forEach(function(elem) {
-								wireContent.appendChild(elem);
-							});
+		match = PATHNAME.match(OWNER_REGEX);
+		if (match) {
+			username = match[1];
+		}
 
-							isLoading = false;
-						}
-					});
+		match = PATHNAME.match(SEARCH_REGEX);
+		if (match) {
+			query = match[1];
+			if (!query) {
+				query = Util.getQueryParameter('q');
+				if (!query) {
+					runScript = false;
 				}
 			}
-		});
+		}
+
+		if (runScript) {
+			var isLoading = false;
+			window.addEventListener('scroll', function(e) {
+				if (!isLoading) {
+					// when we've scrolled past the bottom of the wire content load the stuff
+					if (Posts.postsElement.getBoundingClientRect().bottom <= window.innerHeight) {
+						isLoading = true;
+
+						var params = {
+							'limit': 15,
+							'offset': Posts.getCurrent().length
+						};
+
+						if (friendsOf) {
+							params.friends = friendsOf;
+						}
+
+						if (username) {
+							params.user = username;
+						}
+
+						if (query) {
+							params.query = query;
+						}
+
+						elgg.get(API, {
+							data: params,
+							dataType: 'html',
+							success: function (data) {
+								var tempDiv = document.createElement('div');
+								tempDiv.innerHTML = data;
+
+								var currentPosts = Posts.getCurrent();
+								var oldestPost;
+								if (currentPosts.length > 0) {
+									oldestPost = Math.min.apply(null, currentPosts);
+								}
+
+								var loadedAny = false;
+
+								Util.qq('.elgg-item-object-thewire', tempDiv).filter(function(elem) {
+									// Only include posts that are older than the oldest post
+									if (oldestPost) {
+										var id = Posts.extractId(elem);
+										return id < oldestPost;
+									}
+									return true;
+								}).forEach(function(elem) {
+									loadedAny = true;
+									Posts.postsElement.appendChild(elem);
+								});
+
+								if (loadedAny) {
+									// if no new results, assume at end and stop loading
+									isLoading = false;
+								}
+							}
+						});
+					}
+				}
+			});
+		}
 	}
 })();
