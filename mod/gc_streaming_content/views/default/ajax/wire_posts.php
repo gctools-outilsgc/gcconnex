64e80a23d4d1e6361bd92903fe0a4bf4f4646103
Adrien Pyke <adpyke@gmail.com>
2017-04-07 11:09:05 -0400

    added infinite scroll to other wire views

--- mod/gc_streaming_content/views/default/ajax/wire_posts.php
+++ mod/gc_streaming_content/views/default/ajax/wire_posts.php
@@ -5,16 +5,36 @@
 * Calls The elgg
 */
 
+$query = get_input('query');
+if ($query) {
+	$content = get_wire_entries_by_query($query, get_input('limit'), get_input('offset'));
+} else {
+	$params = array(
+		'type' => 'object',
+		'subtype' => 'thewire',
+		'limit' => get_input('limit'),
+		'offset' => get_input('offset'),
+		'preload_owners' => true,
+		'pagination' =>false,
+	);
+
+	$username = get_input('user');
+	if ($username) {
+		$params['owner_guids'] = array(get_user_by_username($username)->guid);
+	}
+
+	$friends = get_input('friends');
+	if ($friends) {
+		$params['relationship'] = 'friend';
+		$params['relationship_guid'] = get_user_by_username($friends)->guid;
+		$params['relationship_join_on'] = 'container_guid';
+		$content = elgg_list_entities_from_relationship($params);
+	} else {
+		$content = elgg_list_entities($params);
+	}
+}
 
 
-$content = elgg_list_entities(array(
-	'type' => 'object',
-	'subtype' => 'thewire',
-	'limit' => get_input('limit'),
-	'offset' => get_input('offset'),
-	'preload_owners' => true,
-	'pagination' =>false,
-));
 
 echo $content;
 ?>
