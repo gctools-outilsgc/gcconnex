3dc4902d536f9ac0ed5a9ca58c7d44675d4575a4
Nick Schonning <nschonni@gmail.com>
2017-04-07 15:02:12 -0400

    Move gc_streaming_content to composer

--- mod/gc_streaming_content/languages/en.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return array(
-    'stream:new_wire'=>"There are new posts. Click here to load",
-    'stream:new_newsfeed'=>"There are new posts. Click here to load",
-);
--- mod/gc_streaming_content/languages/fr.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return array(
-    'stream:new_wire'=>"Il y a de nouveaux messages. Cliquez ici pour",
-    'stream:new_newsfeed'=>"Il y a de nouveaux messages. Cliquez ici pour",
-);
