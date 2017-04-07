eeb094be429b47f94dbbef17d7eb3fd88a79e833
Nick Schonning <nschonni@gmail.com>
2017-04-07 12:06:23 -0400

    Move wet4 to composer

--- mod/wet4/views/default/page/elements/gsa_view_start.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (elgg_is_active_plugin('gc_fedsearch_gsa') && ((!$gsa_usertest) && strcmp($gsa_agentstring,strtolower($_SERVER['HTTP_USER_AGENT'])) == 0) || strstr(strtolower($_SERVER['HTTP_USER_AGENT']), 'gsa-crawler') !== false )
-	$css = "style='display:none;'";
-else 
-	$css = "";
-?>
-
-<div class='let_crawler_know_to_ignore_this' <?php echo $css ?>>
--- mod/wet4/views/default/thewire_tools/ajax_popup.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$vars = array(
-	'class' => 'mentions-popup hidden',
-	'id' => 'ajax-popup',
-);
-
-echo elgg_view_module('popup', '', elgg_view('graphics/ajax_loader', array('hidden' => false)), $vars);
-
