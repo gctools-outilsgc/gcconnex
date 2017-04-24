b88904feda5b6a3b63d35772bde8e13400d63b3d
Mark Wooff <mark.wooff@gmail.com>
2017-04-24 16:29:30 -0400

    Check for admin

--- mod/gc_profile_nudge/start.php
+++ mod/gc_profile_nudge/start.php
@@ -8,7 +8,7 @@ elgg_register_event_handler('init', 'system', 'gc_profile_nudge_init');
 function gc_profile_nudge_init(){
     elgg_register_ajax_view('gc_profile_nudge/profile_nudge');
 
-    if( elgg_is_logged_in() ){
+    if( elgg_is_logged_in() && !elgg_is_admin_logged_in() ){
 
     	$reminder_time = elgg_get_plugin_setting('reminder_time', 'gc_profile_nudge');
     	$in_days = $reminder_time * 86400; // 1 day = 86400 seconds
