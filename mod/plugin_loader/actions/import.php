8cc73656e443676413586fb47170629062cf3545
Luc Belliveau <luc.belliveau@nrc-cnrc.gc.ca>
2017-05-12 11:22:12 -0300

    renamed plugin

--- mod/plugin_loader/actions/import.php
+++ mod/plugin_loader/actions/import.php
@@ -1,7 +1,7 @@
 <?php
 
-elgg_default_plugin_order_reorder();
-elgg_default_plugin_order_set_status();
+plugin_loader_reorder();
+plugin_loader_set_status();
 
 system_message(elgg_echo('plugin_loader:imported'));
 forward(elgg_get_site_url() . '/admin/plugins');
