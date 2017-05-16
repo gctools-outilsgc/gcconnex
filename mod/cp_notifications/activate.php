52d380ea998b3b0ecdb4c75dd9f5824fcfc23744
pandurx <internalfire5@live.com>
2017-05-15 21:06:53 -0700

    update

--- mod/cp_notifications/activate.php
+++ mod/cp_notifications/activate.php
@@ -4,7 +4,7 @@
  * Setup MySQL databases:
  * please note: when enabling the digest functionality, if the site activity is high, there may be chance of exceeding character limit that can be saved in the database
  * upon enabling module, we will create a new table with column data type that allows for more character storage
- **/
+ */
 
 run_sql_script(__DIR__ . '/install/mysql.sql');
 
@@ -13,7 +13,7 @@ run_sql_script(__DIR__ . '/install/mysql.sql');
  * If there was an old version of cp_notifications installed in the past, check
  * and make sure that the information is migrated to and using the right tables then
  * start doing a clean up of the old data that is saved in the old data table structure
- **/
+ */
 
 
 $query = "SELECT * FROM elggobjects_entity WHERE title LIKE 'Newsletter|%'";
@@ -30,7 +30,6 @@ if (count($newsletter_objects) > 0) {
 		error_log(">> user guid: {$newsletter_title[1]}");
 
 
-
 		// long term fix for the chracter limit, segregate all the entries into records, retrieve via the user guid
 		$newsletter_descriptions = json_decode($newsletter_object->description, true);
 		foreach ($newsletter_descriptions as $newsletter_header => $newsletter_contents) {
@@ -39,10 +38,9 @@ if (count($newsletter_objects) > 0) {
 			if ($newsletter_header !== 'group') {
 				foreach ($newsletter_contents as $content_type => $contents) {
 
-					error_log(">>>> type: {$content_type}");
-
-					foreach ($contents as $content)
-					error_log(">>>>> content guid: {$content_guid} &  content: {$content}");
+					foreach ($contents as $content) {
+						error_log(">>>> type: {$content_type} // content guid: {$content_guid}  // content: {$content}");
+					}
 				}
 			}
 			//$query = "INSERT INTO notification_digest ( user_guid, entry_type, notification_entry ) VALUES ( {$newsletter_title[1]}, '{$newsletter_header}', 'entry' )";
--- mod/cp_notifications/views/default/admin/administer_utilities/notification_troubleshoot.php
+++ mod/cp_notifications/views/default/admin/administer_utilities/notification_troubleshoot.php
@@ -61,4 +61,46 @@ $body .= '</fieldset>';
 
 
 
+
+$query = "SELECT * FROM elggobjects_entity WHERE title LIKE 'Newsletter|%'";
+$newsletter_objects = get_data($query);
+
+if (count($newsletter_objects) > 0) {
+
+    $body .= "-STARTING----------------------------------------------------------------------------------------------- <br/>";
+
+
+    foreach ($newsletter_objects as $newsletter_object) {
+        
+        $newsletter_title = explode('|', $newsletter_object->title);
+        $body .= ">> user guid: {$newsletter_title[1]} <br/>";
+
+
+        // long term fix for the chracter limit, segregate all the entries into records, retrieve via the user guid
+        $newsletter_descriptions = json_decode($newsletter_object->description, true);
+        foreach ($newsletter_descriptions as $newsletter_header => $newsletter_contents) {
+            $body .= ">>> <h2><strong> {$newsletter_header} </strong></h2> <br/>";
+
+            //if ($newsletter_header !== 'group') {
+                foreach ($newsletter_contents as $content_type => $contents) {
+
+                    foreach ($contents as $content_id => $content) {
+                        $body .= ">>>> type: {$content_type} // content guid: {$content_id}  <br/>";
+                    }
+                }
+            //}
+            //$query = "INSERT INTO notification_digest ( user_guid, entry_type, notification_entry ) VALUES ( {$newsletter_title[1]}, '{$newsletter_header}', 'entry' )";
+            //$insert_row = insert_data($query);
+        }
+
+
+        // delete the metadata cpn_newsletter       
+        //$user->cpn_newsletter
+        //$query = "INSERT INTO notification_digest ( user_guid, digest_content ) VALUES ( {$newsletter_title[1]}, {$newsletter_description} )";
+        //$insert_row = insertData();
+    }
+
+    $body .= "-FINISH-----------------------------------------------------------------------------------------------<br/>";
+}
+
 echo elgg_view_module('main', $title, $body);
