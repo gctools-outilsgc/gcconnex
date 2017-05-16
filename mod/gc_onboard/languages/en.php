390b4c03d0d49b36c8a17730fbf4ad1da5619434
ethanWallace <ethan.wallace.91@gmail.com>
2017-05-16 10:50:19 -0400

    New text and layout

--- mod/gc_onboard/languages/en.php
+++ mod/gc_onboard/languages/en.php
@@ -88,13 +88,13 @@ return array(
     'onboard:welcome:three:skip' 	=> "Skip tour",
 
 
-    'onboard:welcome:profile:sell:title' => "But wait there's more!",
+    'onboard:welcome:profile:sell:title' => "Start with a great profile!",
     'onboard:welcome:profile:sell:text' => "Having a complete profile (with photo!), goes a long way to making you more approachable on GCconnex. By filling out your profile and keeping it up to date, you'll make it easier for others to find you and connect with you. Your profile can also act as your online C.V. and help you get oppurtunities.",
-    'onboard:welcome:profile:sell:moveon' => "You can also just get to things and not worry about that stuff right now by clicking done below.",
+    'onboard:welcome:profile:sell:help' => "Let us help you build your profile now in just a few easy steps!",
     'onboard:welcome:profile:sell:img' => "onboard-user-card.jpg",
     'onboard:welcome:profile:sell:img:alt' => "Reach your profile through the user menu.",
-    'onboard:welcome:profile:walkthrough' => 'Go to profile walkthrough',
-    'onboard:welcome:profile:visit' => 'Visit your profile',
+    'onboard:welcome:profile:walkthrough' => 'Build my profile',
+    'onboard:welcome:profile:visit' => 'Visit my profile page',
     'onboard:welcome:profile:access' => 'To access your profile page, click on your avatar and select "My Profile".',
 
     /*Step four - groups*/
--- mod/gc_onboard/views/default/welcome-steps/stepFive.php
+++ mod/gc_onboard/views/default/welcome-steps/stepFive.php
@@ -16,15 +16,16 @@
 </div>
 <div class="panel-body">
     <div class="clearfix">
-        <div class="">
-                  <p><?php echo elgg_echo('onboard:welcome:profile:access'); ?></p>
-          <img class="img-responsive" style="margin: 0 auto 10px;" src="<?php echo elgg_get_site_url() .'mod/gc_onboard/graphics/'.elgg_echo('onboard:welcome:profile:sell:img'); ?>" alt="<?php echo elgg_echo('onboard:welcome:profile:sell:img:alt');?>" title="<?php echo elgg_echo('onboard:welcome:profile:sell:img:alt');?>" />
 
-        </div>
         <div class="">
 
         <p class="mrgn-tp-sm"><?php echo elgg_echo('onboard:welcome:profile:sell:text'); ?></p>
 
+        <div class="mrgn-tp-md">
+                  <p><?php echo elgg_echo('onboard:welcome:profile:access'); ?></p>
+          <img class="img-responsive" style="margin: 15px auto 15px;" src="<?php echo elgg_get_site_url() .'mod/gc_onboard/graphics/'.elgg_echo('onboard:welcome:profile:sell:img'); ?>" alt="<?php echo elgg_echo('onboard:welcome:profile:sell:img:alt');?>" title="<?php echo elgg_echo('onboard:welcome:profile:sell:img:alt');?>" />
+
+        </div>
         <?php
 
         $tour = elgg_view('output/url',array(
@@ -42,13 +43,13 @@
 
 
         ?>
+        <div>
+          <p><?php echo elgg_echo('onboard:welcome:profile:sell:help'); ?></p>
+        </div>
         <div class="text-center mrgn-tp-md mrgn-bttm-md">
           <p><?php echo $tour; ?> or <?php echo $profile; ?></p>
         </div>
 
-        <div class="mrgn-tp-md mrgn-bttm-md">
-          <p><?php echo elgg_echo('onboard:welcome:profile:sell:moveon'); ?></p>
-        </div>
       </div>
     </div>
 
