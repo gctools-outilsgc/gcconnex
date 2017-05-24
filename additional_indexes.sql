8ad513d67c75390a939bd07d25fbab338c2f402b
Phanoix <phanoix@gmail.com>
2017-05-24 16:32:30 -0400

    added queries for the rest of the indexes

--- additional_indexes.sql
+++ additional_indexes.sql
@@ -18,3 +18,27 @@ CREATE INDEX type_enabled ON entities (type,enabled);
 CREATE INDEX type_site_guid ON entities (type,site_guid);
 
 
+-- metadata table
+
+-- metapairs	-> likely the most important one to add, speeds up query by metadata 
+CREATE INDEX metapairs ON metadata (value_id,name_id);
+-- metapairs_plus
+CREATE INDEX metapairs_plus ON metadata (entity_guid,value_id,name_id);
+
+
+-- objects_entity
+
+-- fulltext_title
+CREATE FULLTEXT INDEX fulltext_title ON objects_entity (title);
+-- title_2
+CREATE INDEX title_2 ON objects_entity (title);
+
+
+-- river
+-- obj_tar_ena_post
+CREATE INDEX obj_tar_ena_post ON river (object_guid,target_guid,enabled,posted);
+
+
+
+-- remove access_id index from the entities table - necessary for the newsfeed queries
+DROP INDEX access_id ON entities
