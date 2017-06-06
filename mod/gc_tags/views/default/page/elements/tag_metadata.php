efbfad3ba4ea2e56ad23f87cbeed3d383e6dcfb2
piet0024 <n.pietrantonio0024@live.com>
2017-06-06 13:44:35 -0400

    putting audience into head

--- mod/gc_tags/views/default/page/elements/tag_metadata.php
+++ mod/gc_tags/views/default/page/elements/tag_metadata.php
@@ -1,6 +1,24 @@
 <?php
-
 /*
-*
+* 
+* Extends the site head if the entity has communities or audiences with metadata
 *
 */
+
+//$entity = $vars['entity'];
+global $my_page_entity;
+
+//Does this entity have communities?
+if($my_page_entity->audience){
+    $audience = $my_page_entity->audience;
+    //if multiple communities
+    if(is_array($audience)){
+        foreach($audience as $value){
+            $audience_string .= $value .',';
+        }
+        echo '<meta name="dcterms.audience" content="'.$audience_string.'">';
+    }else{
+        echo '<meta name="dcterms.audience" content="'.$audience.'">';
+    }
+    
+}
