b980cf7e89829971851693feeba04e4af2666f2a
ethanWallace <ethan.wallace.91@gmail.com>
2017-06-06 13:59:28 -0400

    question auto-suggest on creation

--- mod/wet4/actions/object/question/autocomplete.php
+++ mod/wet4/actions/object/question/autocomplete.php
@@ -1,7 +1,34 @@
 <?php
+global $CONFIG;
+$db_prefix = elgg_get_config('dbprefix');
 
 $name = get_input('name');
 $owner = get_input('owner');
+$lang = get_current_language();
+
+$params = array(
+  "type" => "object",
+  'subtype' => "question",
+  'metadata_name_value_pairs' => array(
+      'name' => 'title3',
+      'value' => '%'.$name.'%',
+      'operand' => 'LIKE',
+      'case_sensitive' => false
+    )
+);
+
+$questions = elgg_get_entities_from_metadata($params);
+if($questions){
+  //$content .= '<p><b>Based on your question, we have found similar questions already asked.</b></p>';
+  $content .= '<ul class="list-unstyled mrgn-bttm-0 suggestion-list">';
+  foreach($questions as $q){
+    if($q->getMarkedAnswer()){
+      $check = '<span class="fa fa-check" style="color:#047177"><span class="wb-inv">'.elgg_echo('Answered').'</span></span>';
+    }
+    $content .= '<a target="_blank" href="'.$q->getURL().'"><li>'.$check.gc_explode_translation($q->title3,$lang).'</li></a>';
+  }
+  $content .= '</ul>';
+}
 
 //return question
 echo json_encode([
--- mod/wet4/views/default/input/question_autocomplete.php
+++ mod/wet4/views/default/input/question_autocomplete.php
@@ -10,25 +10,34 @@ echo elgg_view('input/text', array(
   'class' => ''
 ));
 
-echo '<div id="searchResults"></div>'
+echo '<div id="searchResults" aria-live="passive" style="display:none;" class="question-auto"><p><b>Based on your question, we have found similar questions already asked.</b></p></div>'
 ?>
 
 <script>
 
 $(document).ready(function() {
-    $('<?php echo $id; ?>').on('keyup', function(){
+    $('#<?php echo $id; ?>').on('keyup', function(){
       var input = $(this).val();
       if(input.length > 3){
-      elgg.action("question/autocomplete", {
-      data: {
-          name: $(this).val(),
-          owner: <?php echo elgg_get_page_owner_guid(); ?>
-      },
-      success: function (wrapper) {
-
+        elgg.action("question/autocomplete", {
+          data: {
+              name: input,
+              owner: <?php echo elgg_get_page_owner_guid(); ?>
+          },
+          success: function (wrapper) {
+            $('#searchResults .suggestion-list').remove();
+            $('#searchResults').append(wrapper.output.question);
+            if($('#searchResults ul').length){
+              $('#searchResults').css('display', 'block');
+            } else {
+              $('#searchResults').css('display', 'none');
+            }
+          }
+        });
+      } else if(input.length <= 3){
+        $('#searchResults').css('display', 'none');
+        $('#searchResults .suggestion-list').remove();
       }
-      });
-    }
     });
 });
 
