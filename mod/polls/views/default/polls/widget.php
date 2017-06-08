1e77dd30277ae069ed36695609bb128b06b3f5ea
steph4104 <stephanie4104@gmail.com>
2017-06-07 20:13:20 -0500

    update poll widget

--- mod/polls/views/default/polls/widget.php
+++ mod/polls/views/default/polls/widget.php
@@ -8,9 +8,11 @@ $poll = $vars['entity'];
 $owner = $poll->getOwnerEntity();
 $friendlytime = elgg_get_friendly_time($poll->time_created);
 $responses = $poll->countAnnotations('vote');
+$lang = get_current_language();
+$question = gc_explode_translation($poll->question,$lang);
 
 $icon = '<img src="'.elgg_get_site_url().'mod/polls/graphics/poll.png" />';
-$info = "<a href=\"{$poll->getURL()}\">{$poll->question}</a><br>";
+$info = "<a href=\"{$poll->getURL()}\">{$question}</a><br>";
 
 if ($responses == 1) {
 	$noun = elgg_echo('polls:noun_response');
