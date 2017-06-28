50e9151395f2ba1022254f991bbef6a4b51d499d
Phanoix <phanoix@gmail.com>
2017-06-28 14:01:25 -0400

    added group name updates to initial count for update script

--- mod/wet4/views/default/plugins/wet4/settings.php
+++ mod/wet4/views/default/plugins/wet4/settings.php
@@ -25,6 +25,9 @@ if ( $page_mode == 'bilingual_upgrade' ) {
 	$title2_id = elgg_get_metastring_id('title2', true);	// get the metastring id, create the metastring if it does not exist
 	$title3_id = elgg_get_metastring_id('title3', true);
 
+	$name2_id = elgg_get_metastring_id('name2', true);	// get the metastring id, create the metastring if it does not exist
+	$name3_id = elgg_get_metastring_id('name3', true);
+
 	$description2_id = elgg_get_metastring_id('description2', true);	// get the metastring id, create the metastring if it does not exist
 	$description3_id = elgg_get_metastring_id('description3', true);
 
@@ -39,7 +42,7 @@ if ( $page_mode == 'bilingual_upgrade' ) {
 
 	$db_prefix = elgg_get_config('dbprefix');
 	$count = get_data_row("SELECT count(distinct o.guid) as objects from {$db_prefix}objects_entity o LEFT JOIN {$db_prefix}metadata md ON o.guid = md.entity_guid 
-		WHERE md.name_id IN ({$title2_id}, {$title3_id}, {$description2_id}, {$description3_id}, {$briefdescription2_id}, {$briefdescription3_id}, {$excerpt2_id}, {$excerpt3_id}, {$poll_choice2_id}, {$poll_choice3_id})");
+		WHERE md.name_id IN ({$title2_id}, {$title3_id}, {$name2_id}, {$name3_id}, {$description2_id}, {$description3_id}, {$briefdescription2_id}, {$briefdescription3_id}, {$excerpt2_id}, {$excerpt3_id}, {$poll_choice2_id}, {$poll_choice3_id})");
 
 	echo elgg_view('admin/upgrades/view', array(
 		'count' => $count->objects,
