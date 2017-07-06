75e22cded3c613da1f75ec9b6462c36a2243de5b
ethanWallace <ethan.wallace.91@gmail.com>
2017-07-06 15:31:11 -0400

    Mention dropdown now appears again (#1207)
    * Mention dropdown now appears again
    
    Edited our find group member query. Would exit the autocomplete function
    for any other case that wasn't search for croup members.
    
    Also changed what's listed in dropdown slighty (added @username).
    
    * have it still do the checks but without the exit part

--- engine/lib/input.php
+++ engine/lib/input.php
@@ -56,7 +56,7 @@ function filter_tags($var) {
 
 /**
  * Returns the current page's complete URL.
- * 
+ *
  * It uses the configured site URL for the hostname rather than depending on
  * what the server uses to populate $_SERVER.
  *
@@ -212,16 +212,15 @@ function input_livesearch_page_handler($page) {
 	$q = str_replace(array('_', '%'), array('\_', '\%'), $q);
 
 // for the group members search: group guid - should be numeric
-	if (!$g = get_input('term', get_input('g'))) {
-		exit;
+	if (!$g = get_input('term', get_input('g'))){
+		$g=0;
 	}
+	else {
+		$g = sanitise_string($g);
 
-	$input_name = get_input('name', 'members');
-
-	$g = sanitise_string($g);
-
-	// replace mysql vars with escaped strings
-	$g = str_replace(array('_', '%'), array('\_', '\%'), $g);
+		// replace mysql vars with escaped strings
+		$g = str_replace(array('_', '%'), array('\_', '\%'), $g);
+	}
 
 	$match_on = get_input('match_on', 'all');
 
@@ -255,11 +254,11 @@ function input_livesearch_page_handler($page) {
 						"(ue.name LIKE '$q%' OR ue.name LIKE '% $q%' OR ue.username LIKE '$q%')"
 					)
 				);
-				
+
 				$entities = elgg_get_entities($options);
 				if (!empty($entities)) {
 					foreach ($entities as $entity) {
-						
+
 						if (in_array('groups', $match_on)) {
 							$value = $entity->guid;
 						} else {
@@ -301,7 +300,7 @@ function input_livesearch_page_handler($page) {
 				if (!elgg_is_active_plugin('groups')) {
 					continue;
 				}
-				
+
 				$options = array(
 					'type' => 'group',
 					'limit' => $limit,
@@ -311,7 +310,7 @@ function input_livesearch_page_handler($page) {
 						"(ge.name LIKE '$q%' OR ge.name LIKE '% $q%' OR ge.description LIKE '% $q%')"
 					)
 				);
-				
+
 				$entities = elgg_get_entities($options);
 				if (!empty($entities)) {
 					foreach ($entities as $entity) {
@@ -355,11 +354,11 @@ function input_livesearch_page_handler($page) {
 						"(ue.name LIKE '$q%' OR ue.name LIKE '% $q%' OR ue.username LIKE '$q%')"
 					)
 				);
-				
+
 				$entities = elgg_get_entities_from_relationship($options);
 				if (!empty($entities)) {
 					foreach ($entities as $entity) {
-						
+
 						$output = elgg_view_list_item($entity, array(
 							'use_hover' => false,
 							'use_link' => false,
@@ -403,11 +402,11 @@ function input_livesearch_page_handler($page) {
 						"(ue.name LIKE '$q%' OR ue.name LIKE '% $q%' OR ue.username LIKE '$q%')"
 					)
 				);
-				
+
 				$entities = elgg_get_entities_from_relationship($options);
 				if (!empty($entities)) {
 					foreach ($entities as $entity) {
-						
+
 						$output = elgg_view_list_item($entity, array(
 							'use_hover' => false,
 							'use_link' => false,
