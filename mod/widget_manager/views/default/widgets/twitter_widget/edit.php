1f106e8696617944a0c227882cdf488e9772144b
pandurx <internalfire5@live.com>
2017-07-31 20:33:13 -0700

    removed comments

--- mod/widget_manager/views/default/widgets/twitter_widget/edit.php
+++ mod/widget_manager/views/default/widgets/twitter_widget/edit.php
@@ -26,24 +26,3 @@ $height = sanitise_int($widget->height, false);
 </div>
 
 
-<!--
-
-Collection
-<a class="twitter-timeline" href="https://twitter.com/TwitterDev/timelines/539487832448843776">National Park Tweets - Curated tweets by TwitterDev</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-
-Tweet
-Profile
-List
-Likes
-Handles
-Hashtags
-
-
-
-
-
-<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Sunsets don&#39;t get much better than this one over <a href="https://twitter.com/GrandTetonNPS">@GrandTetonNPS</a>. <a href="https://twitter.com/hashtag/nature?src=hash">#nature</a> <a href="https://twitter.com/hashtag/sunset?src=hash">#sunset</a> <a href="http://t.co/YuKy2rcjyU">pic.twitter.com/YuKy2rcjyU</a></p>&mdash; US Dept of Interior (@Interior) <a href="https://twitter.com/Interior/status/463440424141459456">May 5, 2014</a></blockquote> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-
-
--->
-
