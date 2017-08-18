36ecd6c2a68f4b14431e7eab45857b405f7960e5
Phanoix <phanoix@gmail.com>
2017-08-18 17:14:55 -0400

    added plugin activation and ordering to docker install script

--- install/cli/docker_installer.php
+++ install/cli/docker_installer.php
@@ -60,3 +60,122 @@ $installer->batchInstall($params, TRUE);
 // at this point installation has completed (otherwise an exception halted execution).
 echo "Elgg CLI install successful. wwwroot: " . elgg_get_config('wwwroot') . "\n";
 
+
+// arrange and activate mods
+$plugins = array(
+'garbagecollector',
+'groups',
+'logrotate',
+'group_tools',
+'blog',
+'bookmarks',
+'dashboard',
+'diagnostics',
+'friend_request',
+'externalpages',
+'file',
+'embed',
+'group_operators',
+'htmlawed',
+'invitefriends',
+'likes',
+'logbrowser',
+'gcforums',
+'messageboard',
+'messages',
+'polls',
+'profile',
+'rename_friends',
+'reportedcontent',
+'search',
+'tagcloud',
+'tasks',
+'members',
+'thewire',
+'twitter',
+'uservalidationbyemail',
+'unvalidatedemailchange',
+'widget_manager',
+'zaudio',
+'custom_index_widgets',
+'file_tools',
+'event_calendar',
+'login_as',
+'ideas',
+'unvalidated_user_cleanup',
+'pages',
+'twitter_api',
+'analytics',
+'tidypics',
+'translation_editor',
+'sphinx',
+'gcProfilePictureBadges',
+'upload_users',
+'c_email_extensions',
+'gcRegistration',
+'au_subgroups',
+'widget_manager_accessibility',
+'b_extended_profile',
+'c_members_byDepartment',
+'blog_tools',
+'ckeditor',
+'contactform',
+'gc_group_deletion',
+'site_notifications',
+'web_services',
+'missions_profile_extend',
+'missions_organization',
+'missions',
+'custom_error_page',
+'data_views',
+'mt_activity_tabs',
+'geds_sync',
+'gc_api',
+'achievement_badges',
+'embed_extender',
+'toggle_language',
+'thewire_tools',
+'mentions',
+'GoC_dev_banner',
+'wet4',
+'GC_profileStrength',
+'apiadmin',
+'cp_notifications',
+'saml_link',
+'simplesaml',
+'gc_fedsearch_gsa',
+'maintenance',
+'elgg-jsonp',
+'gc_group_layout',
+'gc_official_groups',
+'machine_translation',
+'gc_profile_nudge',
+'phpmailer',
+'gc_onboard',
+'gc_streaming_content',
+'gc_newsfeed',
+'gccollab_stats',
+'gc_splash_page',
+'multi_file_upload',
+'questions',
+'gcconnex_theme'
+);
+
+// activate plugins that are not activated on install, arrange those that are
+foreach ($plugins as $key => $id) {
+	$plugin = elgg_get_plugin_from_id($id);
+
+	if (!$plugin) {
+		unset($plugins[$key]);
+		continue;
+	}
+
+	$plugin->setPriority('last');	// move to the end of the list
+
+	if ($plugin->isActive()){
+		unset($plugins[$key]);
+		continue;
+	}
+
+	$plugin->activate();
+}
