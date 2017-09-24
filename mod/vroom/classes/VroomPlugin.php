6d5f3ee5166cb25ef4f821ae8e0fd3a81f6cbdb5
Mark Wooff <mark.wooff@gmail.com>
2017-09-24 11:49:51 -0400

    Additional mod and JS fix for `gc_tags`

---
+++ mod/vroom/classes/VroomPlugin.php
@@ -0,0 +1,81 @@
+<?php
+/*
+    Plugin Name: VroomPlugin
+    Plugin URI: http://mitre.org/
+    Description: Vroom speeds up elgg
+    Version: 1.0
+    Author: Michael Jett
+    Author URI: http://www.mitre.org/
+    Author Email: mjett@mitre.org
+    License:
+
+      Copyright 2013 MITRE (mjett@mitre.org)
+
+      This program is free software; you can redistribute it and/or modify
+      it under the terms of the GNU General Public License, version 2, as
+      published by the Free Software Foundation.
+
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+
+      You should have received a copy of the GNU General Public License
+      along with this program; if not, write to the Free Software
+      Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+class VroomPlugin
+{
+
+    /**
+     * Class construct
+     */
+    public function __construct()
+    {
+        // Initialize hooks
+        elgg_register_event_handler('init', 'system', array($this, 'init'));
+    }
+
+    /**
+     * Called when the elgg system initializes
+     */
+    public function init()
+    {
+        /*
+         * After elgg finishes an entire system execution, send the output to browser
+         * This allows the other system shutdown processes to continue in the background while output gets returned to the user promptly
+         */
+        elgg_register_event_handler('shutdown', 'system', array($this, 'flushToBrowser'), 0);
+
+    }
+
+    /**
+     * Forces output to the browser so additional php functionality can continue in the background
+     */
+    public function flushToBrowser()
+    {
+        // Registering a shutdown flag allows other points in jettmail to determine if the state is in shutdown
+        $GLOBALS['shutdown_flag'] = 1;
+
+        if (!headers_sent()) {
+
+            // Ignore user aborts and allow the script to run forever
+            ignore_user_abort(true);
+            set_time_limit(0);
+
+            // Tell the browser that we are done
+            header("Connection: close");
+            $size = ob_get_length();
+            header("Content-Length: $size");
+            ob_end_flush();
+            flush();
+        }
+
+    }
+
+
+}
+
+
+
