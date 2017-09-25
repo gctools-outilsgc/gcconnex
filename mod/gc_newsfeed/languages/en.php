56005f8c76862610d45dede7442b1e43f95cf98d
ethanWallace <ethan.wallace.91@gmail.com>
2017-09-25 10:40:46 -0400

    Language changes to news feeds (#1428)
    Add an optional extended description…

--- mod/gc_newsfeed/languages/en.php
+++ mod/gc_newsfeed/languages/en.php
@@ -1,17 +1,17 @@
 <?php
 return array(
     /*
-      * 
+      *
       */
     'newsfeed:menu' => 'News Feed',
 
-    'newsfeed:title'=>'Your News feed',
+    'newsfeed:title'=>'My personal news feed',
     'newsfeed:titlenolog'=>'Welcome',
 
     /*
       * News Feed Filter Language
       */
-    'newsfeed:filter' => "Filter your News Feed",
+    'newsfeed:filter' => "Filter your news feed",
     'newsfeed:label' => "Remove colleague connections from your News Feed",
     'newsfeed:do' => "Colleague connections will no longer display on your News Feed.",
     'newsfeed:undo' => "Colleague connections will now display on your News Feed.",
--- mod/gc_newsfeed/languages/fr.php
+++ mod/gc_newsfeed/languages/fr.php
@@ -5,17 +5,17 @@ return array(
        */
     'newsfeed:menu' => 'Fil de nouvelles',
 
-    'newsfeed:title'=>'Votre fil de nouvelles',
+    'newsfeed:title'=>'Mon fil de nouvelles personnel',
     'newsfeed:titlenolog'=>'Bienvenue',
 
     /*
-      * News Feed Filter Language 
+      * News Feed Filter Language
       */
 
     'newsfeed:filter' => "Filtrer votre fil de nouvelles",
-    'newsfeed:label' => "Enlever les connexions faites avec vos collègues dans votre fil de nouvelles",
-    'newsfeed:do' => "Les connexions faites avec vos collègues n'apparaîtront plus dans votre fil de nouvelles.",
-    'newsfeed:undo' => "Les connexions faites avec vos collègues apparaîtront dans votre fil de nouvelles.",
+    'newsfeed:label' => "Enlever les connexions faites avec vos collÃ¨gues dans votre fil de nouvelles",
+    'newsfeed:do' => "Les connexions faites avec vos collÃ¨gues n'apparaÃ®tront plus dans votre fil de nouvelles.",
+    'newsfeed:undo' => "Les connexions faites avec vos collÃ¨gues apparaÃ®tront dans votre fil de nouvelles.",
     'newsfeed:filter:title' => "Configuration de votre fil de nouvelles",
 
     );
