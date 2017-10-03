92c4d969a244a56087016819cb059e657edb0f70
piet0024 <n.pietrantonio0024@live.com>
2017-10-03 12:18:14 -0400

    adding group ownership to form

--- mod/member_selfdelete/views/default/forms/selfdelete.php
+++ mod/member_selfdelete/views/default/forms/selfdelete.php
@@ -20,15 +20,45 @@ $owned_groups = elgg_get_entities(array(
 ));
 
 if($owned_groups){
+    echo '<div class="row clearfix">';
     foreach($owned_groups as $group){
+        $owner_guid = $group->getOwnerGUID();
+        $members[$owner_guid] = get_entity($owner_guid)->name ."  (@". get_entity($owner_guid)->username .")";
+        echo '<div class="list-break clearfix">';
+        echo '<div class="col-md-8 ">';
         echo elgg_view('group/default', array('entity' => $group));
+        echo '</div><div class="col-md-4 mrgn-tp-md"><div>';
+        echo elgg_view("input/text", array(
+				"id" => "groups-owner-guid",
+				"value" =>  get_entity($owner_guid)->name,
+			));
+
+			echo elgg_view("input/select", array(
+				"name" => "owner_guid",
+				"id" => "groups-owner-guid-select",
+				"value" =>  $owner_guid,
+				"options_values" => $members,
+				"class" => "groups-owner-input hidden",
+			));
+
+			$vars = array(
+				'class' => 'mentions-popup hidden',
+				'id' => 'groupmems-popup',
+			);
+
+			echo elgg_view_module('popup', '', elgg_view('graphics/ajax_loader', array('hidden' => false)), $vars);
+
+			if ($owner_guid == elgg_get_logged_in_user_guid()) {
+				echo "<span class='elgg-text-help'>" . elgg_echo("groups:owner:warning") . "</span>";
+			}
         echo elgg_view('output/url', array(
-            'text'=>'group owner change',
-            'class'=>'btn btn-primary',
-            'href'=>'#',
+            'text'=>'Change',
+            'class'=>'btn btn-primary mrgn-rght-sm elgg-lightbox',
+            'href'=>'ajax/view/member_selfdelete/group_membership_change',
         ));
-        echo '<br>';
+        echo '</div></div></div>';
     }
+    echo '</div>';
 }
 if (elgg_get_plugin_setting('method', PLUGIN_ID) == "choose") {
 	$value = elgg_get_sticky_value('member_selfdelete', 'method', 'delete');
