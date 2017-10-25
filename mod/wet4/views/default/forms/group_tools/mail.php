58dc2424bf764c689e96c2bff5d96acde75de437
pandurx <internalfire5@live.com>
2017-10-25 16:02:31 -0400

    enhance(friend-picker for group mail list): * initial commit for group-mail * fixes #1550

--- mod/wet4/views/default/forms/group_tools/mail.php
+++ mod/wet4/views/default/forms/group_tools/mail.php
@@ -1,41 +1,57 @@
 <?php
 /**
  * Mail group members
- */
- /*
  * GC_MODIFICATION
  * Description: Added accessible labels + fixed url
  * Author: GCTools Team
+ * Modified: Christine Yu
  */
+
+
 $group = $vars["entity"];
 $members = $vars["members"];
 
-$friendpicker_value = array();
-if (!empty($members)) {
-	foreach ($members as $member) {
-		$friendpicker_value[] = $member->getGUID();
-	}
+// CYu: https://elgg.org/discussion/view/2551792/kill-the-friendspicker
+// CYu: Friendspicker input form creates heavy performance load (either server or client side)
+
+$form_data = "";
+
+$member_count = count($members);
+
+$members = array_slice($members, 0, 3);
+
+$textbox = elgg_view('input/text', array('id'=> 'txtSaveChk'));
+
+foreach ($members as $member) {
+	$checkbox = elgg_view('input/checkbox', array(
+		'name' => 	'chkMember',
+		'value' => 	$member->getGUID(),
+		'label' =>	'labelling'));
+	$member_icon = "<img class='img-circle' src='{$member->getIconURL(array('size' => 'small'))}'/>";
+	$display_members .= "<div style='border-bottom:1px solid black; padding:5px 2px 2px 2px;'> {$checkbox} {$member_icon} {$member->getGUID()} / {$member->name}</div>";
 }
 
-$form_data = "<label>" . elgg_echo("group_tools:mail:form:recipients") . ": <span id='group_tools_mail_recipients_count'>" . count($friendpicker_value) . "</span></label>";
-$form_data .= "<br />";
-$form_data .= elgg_view("output/url", array(
-	"text" => elgg_echo("group_tools:mail:form:members:selection"),
-	"href" => "#friends-picker1", // cyu - calling the wrong div, resulting in hiding the whole form
-	"rel" => "toggle"
-));
+$form_data .= " {$textbox}";
+$pagination = "<a id='myLink' href='#' data-key='{\"group_guid\":\"".$group->getGUID()."\", \"page_selected\":\"2\"}'>see page</a>";
 
-$form_data .= "<div id='group_tools_mail_member_selection' >";
-$form_data .= elgg_view("input/friendspicker", array("entities" => $members, "value" => $friendpicker_value, "highlight" => "all", "name" => "user_guids"));
-$form_data .= "</div>";
-$form_data .= "</div>";
+$form_data .= "<section>";
+$form_data .= "<label>Currently displaying {$member_count} group members</label> ";
 
+$form_data .= "
+<br/>
+<div style='border:1px gray solid;'>
+	<div id='display-group-members' style='overflow-y:scroll; height:400px;'>
 
-$form_data .= "<div id='group_tools_mail_member_options'>";
-$form_data .= elgg_view("input/button", array("class" => "elgg-button-action mrs", "value" => elgg_echo("group_tools:clear_selection"), "onclick" => "elgg.group_tools.mail_clear_members();"));
-$form_data .= elgg_view("input/button", array("class" => "elgg-button-action mrs", "value" => elgg_echo("group_tools:all_members"), "onclick" => "elgg.group_tools.mail_all_members();"));
-$form_data .= "<br />";
-$form_data .= "</div>";
+		{$display_members}
+
+	</div>
+	{$pagination}
+</div>
+<br/>
+
+</section>
+";
+
 
 $form_data .= "<div>";
 $form_data .= "<label for='title'>" . elgg_echo("group_tools:mail:form:title") . "</label>";
@@ -53,3 +69,63 @@ $form_data .= elgg_view("input/submit", array("value" => elgg_echo("send")));
 $form_data .= "</div>";
 
 echo $form_data;
+
+
+
+?>
+
+<script>
+
+$(document).on('click', 'input[name="chkMember"]',function() {
+	    var checkbox = $('input[name="chkMember"]');
+
+    $('#txtSaveChk').val(checkbox.val());
+});
+
+var checkboxes = $('input[name="chkMember"]');
+
+checkboxes.on("click", function () {
+//console.log(">>>> checked!!!");
+    // var $checked = checkboxes.filter(":checked"),
+    //     checkedValues = $checked.map(function () {
+    //         return this.value;
+    //     }).get();
+    // $('#txtSaveChk').val(checkedValues);
+});
+
+
+
+$('#myLink').click(function() {
+
+	console.log("hey there");
+	var me = $(this),
+		data = me.data('key');
+
+		console.log(data);
+
+		elgg.action('wet4/group_tools/retrieve_group_members', {
+			data: {
+				'group_guid':data.group_guid,
+				'page_selected':data.page_selected,
+			},
+			success: function(members) {
+
+				for (var item in members.output.display_members){
+					console.log(">>>> " + members.output.display_members[item]);
+					$('#display-group-members').append("<div>"+members.output.display_members[item]+"</div>");
+				}
+
+				console.log("yay!" + JSON.stringify(members));
+			}
+		});
+
+
+});
+
+
+</script>
+
+
+<?php
+
+
