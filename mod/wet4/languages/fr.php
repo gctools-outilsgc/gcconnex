7433a3c0d793e83e2e2d64cfdd58b049bf2b1a11
ethanWallace <ethan.wallace.91@gmail.com>
2017-10-26 10:21:51 -0400

    Added more info for user

--- mod/wet4/languages/fr.php
+++ mod/wet4/languages/fr.php
@@ -172,6 +172,12 @@ return array(
 
 	'form:invalid:extensions' => "Format de fichier non valide. Extensions de fichier autorisées: %s",
 
+	//group invite
+
+	'group:invite:user:moreinfo' => "Users will not appear in drop down list if already a member of the group.",
+
+	////
+
 	'new:dept:activity:title' => "Fil de nouvelles de mon ministère",
 	'dept:activity:show' => "Montrer les connexions de collègue",
 	'dept:activity:hide' => "Cacher les connexions de collègue",
