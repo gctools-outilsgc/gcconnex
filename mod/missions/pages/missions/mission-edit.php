32c7ad1ff758f7f8fa04d93acbf2b5e2f67cd130
Phanoix <phanoix@gmail.com>
2018-02-01 16:52:05 -0500

    duplicating restriction on getting the edit button

--- mod/missions/pages/missions/mission-edit.php
+++ mod/missions/pages/missions/mission-edit.php
@@ -22,7 +22,7 @@ $mission_guid = array_pop($exploded_uri);
 $mission = get_entity($mission_guid);
 
 if(!($mission->owner_guid == elgg_get_logged_in_user_guid() || $mission->account == elgg_get_logged_in_user_guid() || elgg_is_admin_logged_in())
-				|| $mission->state != 'completed' || $mission->state != 'cancelled') {
+				|| $mission->state == 'completed' || $mission->state == 'cancelled') {
     register_error(elgg_echo('limited_access'));
     forward();
 }
