dcd4b142229bccfdbf8b0468ad70d74d5242d570
selemay <smellems@gmail.com>
2018-02-27 12:03:35 -0500

    Update warning text

--- mod/wet4/languages/en.php
+++ mod/wet4/languages/en.php
@@ -147,7 +147,7 @@ return array(
 
     'group:accept:invitation' => 'Accept<span class="wb-inv">group invite to "%s"</span>',
     'group:decline:invitation' => 'Decline<span class="wb-inv">group invite to "%s"</span>',
-    'groups:toomanyfriends' => 'You have too many colleagues to display here!',
+    'groups:toomanyfriends' => 'Looks like you\'re too popular! Your colleagues list can\'t be loaded because you have over 1,000 colleagues.',
 
     'entity:like:link:question' => 'Like the question %s',
     'entity:unlike:link:question' => 'Remove like from the question %s',
--- mod/wet4/languages/fr.php
+++ mod/wet4/languages/fr.php
@@ -143,7 +143,7 @@ return array(
 
 	'group:accept:invitation' => 'Accepter <span class="wb-inv">la demande d\'adhésion au groupe "%s"</span>',
 	'group:decline:invitation' => 'Rejeter <span class="wb-inv"> la demande d\'adhésion au groupe "%s"</span>',
-  'groups:toomanyfriends' => 'Vous avez trop de collègues pour les afficher ici!',
+  'groups:toomanyfriends' => 'Il semble que vous êtes trop populaire! Votre liste de collègues ne peut pas être chargée car vous avez plus de 1 000 collègues.',
 
 	'entity:like:link:question' => 'Aimer la question %s',
 	'entity:unlike:link:question' => 'Enlever la mention j\'aime de la question %s',
