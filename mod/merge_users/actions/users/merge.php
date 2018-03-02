2301fb3d9927b6c324d34e7eb63bffabb16f83f6
ethanWallace <ethan.wallace.91@gmail.com>
2018-03-02 15:03:37 -0500

    Merge account feature (#1743)
    * Event fix + new text
    
    * File transfer test
    
    * Transfer file
    
    Still need to make thumbnails for image files
    
    * Thumbnail creation
    
    * File transfer
    
    Files now transfer properly between accounts
    
    * Added more options to merge
    
    * Added revisions from sprint review
    
    -More clarity in options
    -Option to deactivate old account
    -Remove files, images and albums from content transfer
    
    * Deactivation reasoning
    
    * Handle bookmarks differently
    
    Closes #1629 If anything that needs to change is found we will reopen the issue.

--- mod/merge_users/actions/users/merge.php
+++ mod/merge_users/actions/users/merge.php
@@ -16,7 +16,7 @@ $new_user = get_user_by_username($new);
 
 if(!$old_user || !$new_user){
   register_error('Could not find user.');
-	forward('admin/merge_users/merge');
+	forward('admin/users/merge_users');
 }
 
 $oldGUID = $old_user->guid;
@@ -27,73 +27,119 @@ $edu = get_subtype_id('object', 'education');
 $work = get_subtype_id('object', 'experience');
 $skill = get_subtype_id('object', 'MySkill');
 
+//subtypes to not transfer
+$image = get_subtype_id('object', 'image');
+$file = get_subtype_id('object', 'file');
+$album = get_subtype_id('object', 'album');
+
+//check if we are transfering content
+$transfer_content = get_input('content');
+
+if($transfer_content){
+    //transfering all object entities to new account
+    $data = get_data("SELECT * FROM {$db_prefix}entities WHERE owner_guid = {$oldGUID} AND type='object' AND subtype NOT IN ( $edu, $work, $skill, $image, $file, $album )");
+
+    $event = get_subtype_id('object', 'event_calendar');
+    $bookmark = get_subtype_id('object', 'bookmarks');
+
+    foreach($data as $object){
+
+      if($object->container_guid != $oldGUID){ //entities in a group
+
+        //handle different entitites a certain way
+        switch($object->subtype){
+          case $bookmark:
+            update_data("UPDATE {$db_prefix}metadata SET owner_guid = '$newGUID' where entity_guid = '$object->guid'");
+            break;
+          case $event:
+            add_entity_relationship($newGUID,'personal_event', $object->guid);
+          break;
+          default:
+        }
+
+        update_data("UPDATE {$db_prefix}entities SET owner_guid = '$newGUID' where guid = '$object->guid'");
+
+      } else { //entities under the user
+
+        //handle different entitites a certain way
+        switch($object->subtype){
+          case $bookmark:
+            update_data("UPDATE {$db_prefix}metadata SET owner_guid = '$newGUID' where entity_guid = '$object->guid'");
+            break;
+          case $event:
+            add_entity_relationship($newGUID,'personal_event', $object->guid);
+            break;
+          default:
+        }
+
+        update_data("UPDATE {$db_prefix}entities SET owner_guid = '$newGUID', container_guid = '$newGUID' where guid = '$object->guid'");
+
+      }
+
+    }
+}
+
+
 $transfer_profile = get_input('profile');
 
-//ignore these subtypes if not transfering profile
-if(!$transfer_profile){
-  $avoid = " AND subtype NOT IN ( $edu, $work, $skill )";
-}
-//transfering all object entities to new account
-$data = get_data("SELECT * FROM {$db_prefix}entities WHERE owner_guid = {$oldGUID} AND type='object'".$avoid);
+//handle transfering profile info entities to new user
+if($transfer_profile){
 
-foreach($data as $object){
+  $profile_data = get_data("SELECT * FROM {$db_prefix}entities WHERE owner_guid = {$oldGUID} AND type='object' AND subtype IN ( $edu, $work, $skill )");
 
-  if($object->container_guid != $oldGUID){
-    update_data("UPDATE {$db_prefix}entities SET owner_guid = '$newGUID' where guid = '$object->guid'");
-  } else {
+  foreach($profile_data as $object){
+
     update_data("UPDATE {$db_prefix}entities SET owner_guid = '$newGUID', container_guid = '$newGUID' where guid = '$object->guid'");
 
-    //handle transfering profile info entities to new user
-    if($transfer_profile){
-      switch($object->subtype){
-        //education
-        case $edu:
-          $education = $new_user->education;
+        switch($object->subtype){
+          //education
+          case $edu:
+            $education = $new_user->education;
 
-          if($education == NULL){
-            $new_user->education = $object->guid;
-          } else if(is_array($education)){
-            array_push($education, $object->guid);
-            $new_user->education = $education;
-          } else if(!is_array($education)){
-            $new_user->education = array($education, $object->guid);
-          }
-          update_data("UPDATE {$db_prefix}metadata SET owner_guid = '$newGUID' where entity_guid = '$object->guid'");
-          break;
-        //work experience
-        case $work:
-          $experience = $new_user->work;
-
-          if($experience == NULL){
-            $new_user->work = $object->guid;
-          } else if(is_array($experience)){
-            array_push($experience, $object->guid);
-            $new_user->work = $experience;
-          } else if(!is_array($experience)){
-            $new_user->work = array($experience, $object->guid);
-          }
-          update_data("UPDATE {$db_prefix}metadata SET owner_guid = '$newGUID' where entity_guid = '$object->guid'");
-          break;
-        //skills
-        case $skill:
-          $skills = $new_user->gc_skills;
-
-          if($skills == NULL){
-            $new_user->gc_skills = $object->guid;
-          } else if(is_array($skills)){
-            if(count($skills) < 15){ //max 15 skill
-              array_push($skills, $object->guid);
-              $new_user->gc_skills = $skills;
+            if($education == NULL){
+              $new_user->education = $object->guid;
+            } else if(is_array($education)){
+              array_push($education, $object->guid);
+              $new_user->education = $education;
+            } else if(!is_array($education)){
+              $new_user->education = array($education, $object->guid);
             }
-          } else if(!is_array($skills)){
-            $new_user->gc_skills = array($skills, $object->guid);
-          }
+            update_data("UPDATE {$db_prefix}metadata SET owner_guid = '$newGUID' where entity_guid = '$object->guid'");
+            break;
+          //work experience
+          case $work:
+            $experience = $new_user->work;
 
-          break;
+            if($experience == NULL){
+              $new_user->work = $object->guid;
+            } else if(is_array($experience)){
+              array_push($experience, $object->guid);
+              $new_user->work = $experience;
+            } else if(!is_array($experience)){
+              $new_user->work = array($experience, $object->guid);
+            }
+            update_data("UPDATE {$db_prefix}metadata SET owner_guid = '$newGUID' where entity_guid = '$object->guid'");
+            break;
+          //skills
+          case $skill:
+            $skills = $new_user->gc_skills;
+
+            if($skills == NULL){
+              $new_user->gc_skills = $object->guid;
+            } else if(is_array($skills)){
+              if(count($skills) < 15){ //max 15 skill
+                array_push($skills, $object->guid);
+                $new_user->gc_skills = $skills;
+              }
+            } else if(!is_array($skills)){
+              $new_user->gc_skills = array($skills, $object->guid);
+            }
+
+            break;
+        }
       }
-    }
-  }
 }
+
 
 //transfering group ownership and making sure new account is a member of the group they are now the owner of
 $dataGroups = get_data("SELECT * FROM {$db_prefix}entities WHERE owner_guid = {$oldGUID} AND type='group'");
@@ -107,8 +153,6 @@ foreach($dataGroups as $group){
   if(!$groupEnt->isMember($new_user)){
     $groupEnt->join($new_user);
   }
-
-
 
   //cover photo
   if(elgg_is_active_plugin('gc_group_layout')){
@@ -154,13 +198,22 @@ if($transfer_friends){
 
 }
 
-system_message('All content and groups has been transfered to '.$new_user->name.' and the account '.$old_user->name);
+system_message('The account <b>'.$new_user->name.'</b> has been merged into the account <b>'.$old_user->name.'</b>.');
 
 //unset from old account so entities are not deleted as well
 if($transfer_profile){
   unset($old_user->education);
   unset($old_user->work);
   unset($old_user->gc_skills);
+}
+
+//Test for deactivate
+$deactivate = get_input('deactivate');
+
+if($deactivate){
+  $old_user->gcdeactivate = true;
+  $old_user->gcdeactivatereason = "Merged account";
+  $old_user->gcdeactivatetime = time();
 }
 
 //lets say goodbye to this old user
