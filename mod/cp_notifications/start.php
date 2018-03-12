d0e30cc811c16116f5d0ee6f885fb73725653521
pandurx <internalfire5@live.com>
2018-03-12 11:38:49 -0400

    return true, has been moved after the foreach loop when it has gone through all the watchers

--- mod/cp_notifications/start.php
+++ mod/cp_notifications/start.php
@@ -757,9 +757,8 @@ function cp_create_annotation_notification($event, $type, $object) {
 					if (check_entity_relationship($watcher->guid, 'cp_subscribed_to_site_mail', $entity->getContainerGUID()))
 						messages_send($subject, $template, $watcher->guid, $site->guid, 0, true, false);
 				}
-
-				return true;
 			}
+			return true;
 		}
 
 
@@ -787,25 +786,28 @@ function cp_create_annotation_notification($event, $type, $object) {
 
 			foreach ($watchers as $watcher) {
 				$message['user_name'] = $watcher->username;
-
 				$template = elgg_view('cp_notifications/email_template', $message);
 				$recipient_user = get_user($watcher->guid);
 
-				if (has_access_to_entity($entity, $recipient_user) && $object->access_id != 0) {
+				if (has_access_to_entity($entity, $recipient_user) /*&& $object->access_id != 0*/) {
 
-					if (strcmp(elgg_get_plugin_user_setting('cpn_set_digest', $watcher->guid,'cp_notifications'),'set_digest_yes') == 0)
+					if (strcmp(elgg_get_plugin_user_setting('cpn_set_digest', $watcher->guid,'cp_notifications'),'set_digest_yes') == 0) {
+
 						create_digest($author, $action_type, $content_entity, get_entity($watcher->guid));
-					else
+					}
+					else {
+
 						(elgg_is_active_plugin('phpmailer')) ? phpmailer_send( $watcher->email, $watcher->name, $subject, $template, NULL, true ) : mail($watcher->email, $subject, $template, cp_get_headers());
+					}
+
 
 					if (check_entity_relationship($watcher->guid, 'cp_subscribed_to_site_mail', $entity->getContainerGUID())) {
 						$site_template = elgg_view('cp_notifications/site_template', $message);
 						messages_send($subject, $site_template, $watcher->guid, $site->guid, 0, true, false);
 					}
 				}
-			
-				return true;
 			}
+			return true;
 		}
 		
 	} else {
