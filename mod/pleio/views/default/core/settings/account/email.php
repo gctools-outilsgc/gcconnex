55becb3320ffaa2080d78636336303b2ba961981
Mark Wooff <mark.wooff@gmail.com>
2018-03-15 13:49:23 -0400

    Wording changes to user settings

--- mod/pleio/views/default/core/settings/account/email.php
+++ mod/pleio/views/default/core/settings/account/email.php
@@ -1,13 +1,18 @@
 <?php
+$auth = elgg_get_plugin_setting('auth', 'pleio');
 $auth_url = elgg_get_plugin_setting('auth_url', 'pleio');
+
+if ($auth == 'oidc') {
+	$auth_url = str_replace("openid", "", $auth_url);
+}
 
 $user = elgg_get_page_owner_entity();
 if ($user) {
     $title = elgg_echo("pleio:change_settings");
     
     $link = elgg_view("output/url", [
-        "href" => $auth_url . "settings/user",
-        "text" => "Pleio",
+        "href" => $auth_url . "profile",
+        "text" => "GCcollab Account",
         "target" => "_blank"
     ]);
 
