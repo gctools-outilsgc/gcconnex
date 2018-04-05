87bfb8a38c2983c1712dd065f4b4e0ee3b4a7e7c
BrandonGould <34379222+bmrgould@users.noreply.github.com>
2018-04-05 15:28:05 -0400

    Minor - PostBlog Mobile API
    Fixed missing arrows issue...

--- mod/gc_mobile_api/models/test.php
+++ mod/gc_mobile_api/models/test.php
@@ -2,7 +2,8 @@
 /*
  * Exposes API endpoints for Blog entities
  */
-//https://github.com/gctools-outilsgc/gcconnex/blob/master/mod/blog/actions/blog/save.php
+// https://github.com/gctools-outilsgc/gcconnex/blob/master/mod/blog/actions/blog/save.php
+// https://github.com/gctools-outilsgc/gcconnex/blob/master/mod/blog_tools/actions/blog/save.php
 
  elgg_ws_expose_function(
  	"post.blog",
@@ -13,9 +14,9 @@
 		"excerpt" => array('type' =>'string', 'required' => false, 'default' => ''),
  		"body" => array('type' =>'string', 'required' => false, 'default' => ''),
 		"container_guid" => array('type' =>'string', 'required' => false, 'default' => ''),
-    "comments" = array('type' =>'int', 'required' => false, 'default' => 1),
-    "access" = array('type' =>'int', 'required' => false, 'default' => 1),
-    "status" = array('type' =>'int', 'required' => false, 'default' => 0),
+    "comments" => array('type' =>'int', 'required' => false, 'default' => 1),
+    "access" => array('type' =>'int', 'required' => false, 'default' => 1),
+    "status" => array('type' =>'int', 'required' => false, 'default' => 0),
  		"lang" => array('type' => 'string', 'required' => false, 'default' => "en")
  	),
  	'Posts a new blog post',
@@ -24,7 +25,7 @@
  	false
  );
 
- function post_blog($user, $title, $body, $excerpt, $container_guid, $comments, $access, $status, $lang)
+ function post_blog($user, $title, $excerpt, $body, $container_guid, $comments, $access, $status, $lang)
  {
  	$user_entity = is_numeric($user) ? get_user($user) : (strpos($user, '@') !== false ? get_user_by_email($user)[0] : get_user_by_username($user));
   	if (!$user_entity) {
@@ -37,31 +38,19 @@
   		login($user_entity);
   	}
 
-		//check inputs first then convert from json
-		if (($title == '' || $body == '')){
-			return "fail inputs";
-		}
+		//check required fields being not empty
+		if (($title == '' || $body == '')){ return "Missing required fields (title, or body)"; }
+
 
 		//If no group container, use user guid.
 		if ($container_guid==''){ $container_guid = $user_entity->guid; }
-
-		//default values
-		$values = array(
-			'title' => '',
-			'description' => '',
-			'status' => 'draft',
-			'access_id' => ACCESS_DEFAULT,
-			'comments_on' => 'On',
-			'excerpt' => '',
-			'tags' => '',
-			'container_guid' => $container_guid,
-		);
 
     return elgg_echo("test: pre blog creation");
 
 		//Create blog
 		$blog = new ElggBlog();
 		$blog->subtype = 'blog';
+    $blog->container_guid = $container_guid;
 		$new_post = TRUE;
 
 
