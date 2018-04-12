7e01fb799bf1f67ce094b8ba2966b046bc7d3758
christine <internalfire5@live.com>
2018-04-12 14:54:08 -0400

    * cleaned up code * language toggle will now work via GET and cookie selection * removed duplicate javascript function definition

--- mod/toggle_language/views/default/toggle_language/toggle_lang.php
+++ mod/toggle_language/views/default/toggle_language/toggle_lang.php
@@ -1,65 +1,42 @@
-<div>
-	<?php
-		global $SESSION;
-	?>
 
-	<div class="lang_toggle">
+	<?php global $SESSION; ?>
+
 	<script type="text/javascript">
+		
 		function form_submit(language_selected) {
-			//document.getElementById('formtoggle').submit();
+			var cookie_name = "connex_lang";
+			var cookie_value = getCookie(cookie_name);
 
-			var c_name = "connex_lang";
-			var c_value = document.cookie;
-			var c_start = c_value.indexOf(" " + c_name + "=");
-			
-			//alert("check1");
-			if (c_start == -1){
-				c_start = c_value.indexOf(c_name + "=");
-			}
+			<?php // get the language parameter ?>
+			var url_string = window.location.href;
+			var url = new URL(url_string);
+			var language_param = url.searchParams.get("language");
 
-			if (c_start == -1) 
+			if (language_param != null) cookie_value = language_param;
+
+			<?php // first time you visit the site.. the cookie language is set to null ?>
+			if (cookie_value == "")
 			{
-				c_value = null;
+				<?php // check what the user selected ?>
+				var cookie_language = (language_selected == "French") ? "en" : "fr";
+				set_cookie(cookie_name, cookie_language);
+				parent.location.reload(true);
+
 			} else {
 
-				c_start = c_value.indexOf("=", c_start) + 1;
-				var c_end = c_value.indexOf(";", c_start);
-				if (c_end == -1) {
-					c_end = c_value.length;
-				}
-				c_value = unescape(c_value.substring(c_start,c_end));
-			}
+				<?php // if the cookie was set to english, the user selected french ?>
+				var cookie_language = (cookie_value == "en") ? "fr" : "en";
+				set_cookie(cookie_name, cookie_language);
 
-			//alert("language is:" + c_value);
-			//set_cookie('cclang')
-
-			// modified by cyu - oct 1 2013
-			// first time you visit the site.. the cookie language is set to null
-			if (c_value == null)
-			{
-				//alert("null is caught..");
-				// we need to check what the user selected...
-				if (language_selected == "English")
-				{
-					set_cookie(c_name, "en");
-
-				} else 
-				if (language_selected == "French")
-				{
-					set_cookie(c_name,"fr");
-				}
-
-				parent.location.reload(true);
-			} else {
-
-				if (c_value == "en"){
-					set_cookie(c_name,"fr");
-				} else {
-					set_cookie(c_name, "en");
-				}
-				parent.location.reload(true);
+				<?php // if the language parameter in the url is set, emulate user clicking on link to change language ?>
+				if (language_param == null)
+					parent.location.reload(true);
+				
+				else 
+					window.location.href = url_string.replace("language=" + cookie_value, "language=" + cookie_language);
 			}
 		}
+		
 		function set_cookie(name,value) {
 			var today = new Date();
 			today.setTime( today.getTime() );
@@ -67,43 +44,45 @@
 			var expires_date = new Date( today.getTime() + (expires) );
 			document.cookie = name + "=" +escape( value ) + ";path=/" + ";expires=" + expires_date.toGMTString();
 		}
+
+		function getCookie(cname) {
+		    var name = cname + "=";
+		    var decodedCookie = decodeURIComponent(document.cookie);
+		    var ca = decodedCookie.split(';');
+		    
+		    for (var i = 0; i < ca.length; i++) {
+		        var c = ca[i];
+		        while (c.charAt(0) == ' ') {
+		            c = c.substring(1);
+		        }
+		        if (c.indexOf(name) == 0) {
+		            return c.substring(name.length, c.length);
+		        }
+		    }
+		    return "";
+		}
 		
-	
 	</script>
-		<form action="<?php echo $vars['url']; ?>action/toggle_language/toggle" method="post" id="formtoggle">
-			
-		<?php
-		// security tokens.
-		echo elgg_view('input/securitytoken');
-		?>
-		</form>
 		
-		<?php
-			//$log = fopen(dirname( __FILE__ ) . "/cyu - session conditional statement.txt", 'w');
-			//fwrite($log, "start logging - session language" . "\r\n" );
 
-			if ($SESSION['language'] == 'en') { 
-			//if ($_COOKIE["cc_lang"] == 'en'){
-				//fwrite($log, "session is in english" . "\r\n" );
+	<div>
+		<div class="lang_toggle">
+	
+			<form action="<?php echo $vars['url']; ?>action/toggle_language/toggle" method="post" id="formtoggle">
+				<?php echo elgg_view('input/securitytoken'); ?>
+			</form>
+			
+			<?php if ($SESSION['language'] == 'en') { ?>
+					<span class="active">English</span>
+					<a class="not_active" href="javascript:form_submit('French')"><span lang="fr">Fran&ccedil;ais</span></a>
 
-		?>
-				<span class="active">English</span>
-				<a class="not_active" href="javascript:form_submit('French')"><span lang="fr">Fran&ccedil;ais</span></a>
+			<?php } else { ?>
+					<a class="not_active" href="javascript:form_submit('English')"><span lang="en">English</span></a>
+					<span class="active">Fran&ccedil;ais</span>
 
+			<?php } ?>
 
-		<?php
-			} else { //if ($SESSION['language'] == 'fr') {
-				//fwrite($log, "session is in french" . "\r\n" );
-		?>
-				<a class="not_active" href="javascript:form_submit('English')"><span lang="en">English</span></a>
-				<span class="active">Fran&ccedil;ais</span>
-
-		<?php
-			}
-
-			//fwrite($log, "end logging - session language" . "\r\n" );
-			//fclose($log);
-		?>
+		</div>
 	</div>
 
-</div>
+	
