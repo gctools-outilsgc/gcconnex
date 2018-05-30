74c25329aed2b5bb5c2105a2cacb4eaab5420aa8
Steph4104 <stephanie4104@gmail.com>
2018-05-30 09:54:36 -0400

    add event function

--- mod/gc_mobile_api/models/event.php
+++ mod/gc_mobile_api/models/event.php
@@ -93,6 +93,27 @@ elgg_ws_expose_function(
 	true,
 	false
 );
+
+elgg_ws_expose_function(
+	"save.event",
+	"save_event",
+	array(
+		"user" => array('type' => 'string', 'required' => true),
+		"title" => array('type' => 'string', 'required' => true),
+		"excerpt" => array('type' =>'string', 'required' => false, 'default' => ''),
+		"body" => array('type' =>'string', 'required' => true),
+		"container_guid" => array('type' =>'string', 'required' => false, 'default' => ''),
+		"blog_guid" => array('type' =>'string', 'required' => false, 'default' => ''),
+		"comments" => array('type' =>'int', 'required' => false, 'default' => 1),
+		"access" => array('type' =>'int', 'required' => false, 'default' => 1),
+		"status" => array('type' =>'int', 'required' => false, 'default' => 0),
+		"lang" => array('type' => 'string', 'required' => false, 'default' => "en")
+	),
+	'Posts/Saves an event post',
+	'POST',
+	true,
+	false
+   );
 function get_event($user, $guid, $lang)
 {
 	$user_entity = is_numeric($user) ? get_user($user) : (strpos($user, '@') !== false ? get_user_by_email($user)[0] : get_user_by_username($user));
@@ -491,4 +512,123 @@ function get_see_calendar($user, $guid, $lang)
 
 	return $data;
 
+}
+
+
+function save_event($user, $title, $excerpt, $body, $container_guid, $event_guid, $comments, $access, $status, $lang)
+{
+ $user_entity = is_numeric($user) ? get_user($user) : (strpos($user, '@') !== false ? get_user_by_email($user)[0] : get_user_by_username($user));
+	 if (!$user_entity) {
+		 return "User was not found. Please try a different GUID, username, or email address";
+	 }
+	 if (!$user_entity instanceof ElggUser) {
+		 return "Invalid user. Please try a different GUID, username, or email address";
+	 }
+	 if (!elgg_is_logged_in()) {
+		 login($user_entity);
+	 }
+	 $error = FALSE;
+	 //check required fields being not empty
+	 $titles = json_decode($title);
+	 $bodies = json_decode($body);
+	 $excerpts = json_decode($excerpt);
+	 //Check Required
+	 if (!$titles->en && !$titles->fr) { return elgg_echo("blog:error:missing:title"); }
+	 if (!$bodies->en && !$bodies->fr) { return elgg_echo("blog:error:missing:description");  }
+	 if (!($titles->en && $bodies->en) && !($titles->fr && $bodies->fr)) { return "require-same-lang"; }
+	 //Default any Missing or faulty
+	 if (!$titles->en) { $titles->en = ''; }
+	 if (!$titles->fr) { $titles->fr = ''; }
+	 if (!$bodies->en) { $bodies->en = ''; }
+	 if (!$bodies->fr) { $bodies->fr = ''; }
+	 if (!$excerpts->en) { $excerpts->en = ''; }
+	 if (!$excerpts->fr) { $excerpts->fr = ''; }
+	 if ($comments != 0 && $comments != 1) { $comments = 1; }
+	 if ($access != 0 && $access != 1 && $access != -2 && $access !=2 ) { $access = 1; }
+	 if ($status != 0 && $status != 1) { $status = 0; }
+
+	 // if there is a container_guid, .: group, and access is set to group only, set access to proper group only
+	 if (!empty($container_guid) && $access == 2){
+		 $container = get_entity($container_guid);
+		 //validate container and ability to write to it
+		 if (!$container || !$container->canWriteToContainer(0, 'object', 'blog')) {
+ 			return elgg_echo('blog:error:cannot_write_to_container');
+ 		} else {
+			$access = $container->group_acl;
+		}
+		 //If no group container, use user guid.
+	 } else if ($container_guid=='') { $container_guid = $user_entity->guid; }
+
+	 //Set int variables to correct
+	 if ($status == 1) { $status = 'published'; } else { $status = 'draft'; }
+	 if ($comments == 1) { $comments = 'On'; } else { $comments = 'Off'; }
+	 if ($status == 'draft') { $access = 0; }
+	 $titles->en = htmlspecialchars($titles->en, ENT_QUOTES, 'UTF-8');
+	 $titles->fr = htmlspecialchars($titles->fr, ENT_QUOTES, 'UTF-8');
+	 $excerpts->en = elgg_get_excerpt($excerpts->en);
+	 $excerpts->fr = elgg_get_excerpt($excerpts->fr);
+
+	 $values = array(
+		 'title' => JSON_encode($titles),
+		 'title2' => '',
+		 'description' => JSON_encode($bodies),
+		 'description2' => '',
+		 'status' => $status,
+		 'access_id' => $access,
+		 'comments_on' => $comments,
+		 'excerpt' => JSON_encode($excerpts),
+		 'excerpt2' => '',
+		 'tags' => '',
+		 'publication_date' => '',
+		 'expiration_date' => '',
+		 'show_owner' => 'no'
+	 );
+
+	 $event = new stdClass();
+	 $revision_text = '';
+	 if ($event_guid){
+		 $entity = get_entity($event_guid);
+		 if (elgg_instanceof($entity, 'object', 'event') && $entity->canEdit()) {
+			 	$event = $entity;
+		 } else {
+			 return elgg_echo('blog:error:post_not_found');
+		 }
+		 $revision_text = $event->description;
+		 $new_post = $event->new_post; //what?
+	 } else {
+		 //Create blog
+		 $event = new ElggEvent();
+		 $event->subtype = 'event';
+		 $event->container_guid = $container_guid;
+		 $new_post = TRUE;
+	 }
+
+	 $old_status = $event->status;
+
+	 // assign values to the entity, stopping on error.
+	 if (!$error) {
+		 foreach ($values as $name => $value) {
+			 if (($name != 'title2') && ($name != 'description2') &&  ($name != 'excerpt2')){ // remove input 2 in metastring table
+			 $event->$name = $value;
+			 }
+		 }
+	 }
+
+	 if (!$error){
+		 if ($event->save()){
+
+			if (!$event_guid && $event->web_conference) {
+				if (!event_calendar_create_bbb_conf($event)) {
+					register_error(elgg_echo('event_calendar:conference_create_error'));
+				}
+			}
+			if ($group_guid && (elgg_get_plugin_setting('autogroup', 'event_calendar') == 'yes')) {
+				event_calendar_add_personal_events_from_group($event->guid, $group_guid);
+			}
+		}
+		return $event;
+
+	} else {
+		return elgg_echo('blog:error:cannot_save');
+	}
 }
