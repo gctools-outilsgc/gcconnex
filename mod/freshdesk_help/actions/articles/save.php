0ba3ebcb120dbc05b2f69350f4c95f51c3997863
Phanoix <phanoix@gmail.com>
2018-08-10 01:35:48 -0400

    test with hardcoded path

--- mod/freshdesk_help/actions/articles/save.php
+++ mod/freshdesk_help/actions/articles/save.php
@@ -1,5 +1,5 @@
 <?php
-$myFile = elgg_get_config("dataroot") . "freshdesk_help/articles.json";
+$myFile = "/sites/gccollab_data/freshdesk_help/articles.json";
 $fh = fopen($myFile, 'w') or die("can't open file");
 $stringData = $_POST["html"];
 fwrite($fh, $stringData);
