74ebac8f5dfcbe8d01b981727eae8a8718aaff38
Ilia <phanoix@gmail.com>
2018-09-13 10:14:57 -0400

    Update functions.php

--- mod/cp_notifications/lib/functions.php
+++ mod/cp_notifications/lib/functions.php
@@ -987,7 +987,7 @@ function initialize_queue( $frequency ){
 		# get user guid list and insert them all into the queue  ... maybe get it to filter by $frequency too
 		$query = "INSERT INTO notification_digest_queue (user_guid) SELECT entity_guid as user_guid FROM {$dbprefix}private_settings WHERE name = 'plugin:user_setting:cp_notifications:cpn_set_digest' AND value = 'set_digest_yes'";
 		$result = insert_data($query);
-	} catch (Exception $e) {// let mysql take care of duplicate instert attempts, it's good at it}
+	} catch (Exception $e) {/* let mysql take care of duplicate instert attempts */}
 
 	return 0;
 }
