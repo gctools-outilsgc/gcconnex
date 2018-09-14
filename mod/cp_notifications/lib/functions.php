56dc9172cc069ff56c1efb955424811c649e0089
Ilia <phanoix@gmail.com>
2018-09-13 20:20:25 -0400

    add sleep(60) to queue init

--- mod/cp_notifications/lib/functions.php
+++ mod/cp_notifications/lib/functions.php
@@ -988,7 +988,9 @@ function initialize_queue( $frequency ){
 		$query = "INSERT INTO notification_digest_queue (user_guid) SELECT entity_guid as user_guid FROM {$dbprefix}private_settings WHERE name = 'plugin:user_setting:cp_notifications:cpn_set_digest' AND value = 'set_digest_yes'";
 		$result = insert_data($query);
 	} catch (Exception $e) {/* let mysql take care of duplicate instert attempts */}
-
+	
+	sleep(60);		// can certainly be done in a better way, but this is simplest and is unlikely to cause duplicates or unsent digests.
+	
 	return 0;
 }
 
