c81fa4422b3d767e3eb24911fde184383fe7182f
Phanoix <phanoix@gmail.com>
2019-06-20 15:55:36 -0400

    started a new mod to help with load testing

---
+++ mod/gcloadtest/start.php
@@ -0,0 +1,32 @@
+<?php
+
+elgg_register_event_handler('init','system','loadtest_init');
+
+function loadtest_init($event, $object_type, $object = null) {
+    /* page handlers */
+    // for generating random test content
+    elgg_register_page_handler('gen-content', 'generate_content_page_handler');
+
+    // for simulating page load - related system load
+}
+
+function generate_content_page_handler($params) {
+    // generate content of type requested
+    switch ($params[0]) {   // first parameter is the content type
+        case 'blog':
+        case 'blogs':
+            createBlogs($params[1]);
+            break;
+        // the rest go here
+        default:
+            # nothing
+            echo "usage:  .../gen-content/[blogs, bookmarks, discussions, groups, members, wire_posts, files, polls, events, missions]/N  to create N random instances of that content type ";
+            break;
+    }
+}
+
+
+
+function createBlogs($N) {
+    // create N blogs efficiently
+}
