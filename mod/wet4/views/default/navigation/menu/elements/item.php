37869ce42a3b2a2e8f3df9e54a5a84878b7b858d
piet0024 <n.pietrantonio0024@live.com>
2019-08-01 11:32:37 -0400

    WIP gcconnex theme and navigation views combed through

--- mod/wet4/views/default/navigation/menu/elements/item.php
+++ mod/wet4/views/default/navigation/menu/elements/item.php
@@ -37,7 +37,6 @@ if (isset($vars['item_class']) && $vars['item_class']) {
 }
 
 echo "<li class=\"$item_class\">";
-//echo "<li>";
 echo elgg_view_menu_item($item);
 if ($children) {
 	echo elgg_view('navigation/menu/elements/section', array(
