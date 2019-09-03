52be615a0a7d135d280bc4976a9df709883b98d9
Ethan Wallace <ethan.wallace.91@gmail.com>
2019-09-03 11:37:31 -0400

    Fixed mobile layout of files
    removed new file folder title menu button on file view page

--- mod/wet4/views/default/object/elements/summary.php
+++ mod/wet4/views/default/object/elements/summary.php
@@ -186,10 +186,10 @@ if($entity instanceof ElggEntity && $entity->getType() == 'group' ){
 
 	}else{
 
-	echo '<div class="row mrgn-tp-sm">';
+	echo '<div class="mrgn-tp-sm">';
 	if ($metadata) {
 	    if ($checkPage != 'widgets_calendar'){
-	        echo '<div class="col-xs-12 mrgn-lft-md ">' .$metadata . '</div>';
+	        echo '<div class="col-xs-12">' .$metadata . '</div>';
     	}
 	}
 
