cc67cfd3fcf29773b6ac58a2766c87f25ac67c44
ethanWallace <ethan.wallace.91@gmail.com>
2019-09-09 09:26:08 -0400

    Update GCcollab mobile layout style (#2052)
    * Fix GCcollab mobile header
    
    * Fixed mobile layout of files
    
    removed new file folder title menu button on file view page

--- mod/gccollab_theme/views/default/page/elements/site-brand.php
+++ mod/gccollab_theme/views/default/page/elements/site-brand.php
@@ -26,13 +26,12 @@ if (elgg_is_active_plugin('gc_fedsearch_gsa') && ((!$gsa_usertest) && strcmp($gs
 ?>
 
     <div id="app-brand">
-        <div class="container">
+        <div class="container hidden-sm hidden-xs">
             <div class="row">
-                <div class="brand col-xs-8 col-sm-5 col-md-4 col-lg-4">
-                   <object type="image/svg+xml" tabindex="-1" data="<?php echo $site_url ?>/mod/wet4_collab/graphics/sig-blk-<?php echo $graphic_lang; ?>.svg"></object>
-                    <span class="wb-inv"><?php echo elgg_echo('wet:gc');?></span>
+                <div class="brand hidden-sm hidden-xs col-xs-8 col-sm-5 col-md-4 col-lg-4">
+                   <object type="image/svg+xml" tabindex="-1" data="<?php echo $site_url ?>/mod/wet4_collab/graphics/sig-blk-<?php echo $graphic_lang; ?>.svg" aria-label="<?php echo elgg_echo('wet:gc');?>"></object>
                 </div>
-                <section class="hidden-xs col-sm-7 col-md-6 col-lg-6">
+                <section class="hidden-sm hidden-xs col-sm-7 col-md-6 col-lg-6">
                     <ul id="" class="pull-left list-unstyled mrgn-bttm-0" style="font-weight:bold;">
                         <li class="pull-left tool-link">
                         <a href="https://account.gccollab.ca" style="color:#6b5088;">
@@ -51,42 +50,47 @@ if (elgg_is_active_plugin('gc_fedsearch_gsa') && ((!$gsa_usertest) && strcmp($gs
                         </li>
                     </ul>
                 </section>
-                <section class="wb-mb-links visible-xs col-xs-4 col-sm-3" id="wb-glb-mn">
-                    <h2><?php echo elgg_echo('wet:search');?></h2>
-                    <div class="container row purple-bg mbm">
-                        <div class="col-xs-6 app-name">
-                            <a href="<?php echo $site_url; ?>"><span><span class="bold-gc">GC</span>collab</span></a>
-                        </div>
-                        <div class="col-xs-6 icons text-right">
-                            <a href="#mb-pnl" title="<?php echo elgg_echo('wet:search');?>" aria-controls="mb-pnl" class="overlay-lnk" role="button">
-                                <span class="glyphicon glyphicon-search mrl"><span class="wb-inv"><?php echo elgg_echo('wet:search');?></span></span>
-                                <span class="glyphicon glyphicon-th-list"><span class="wb-inv"><?php echo elgg_echo('wet:menu');?></span></span>
-                            </a>
-                        </div>
-                    </div>
-                    <div id="mb-pnl"></div>
-                </section>
+            
                 <div class="col-sm-3 col-md-2 col-lg-2">
                     <?php echo elgg_view('page/elements/chng-lang', $vars); ?>
                 </div>
             </div>
+        </div>
+
+        <section class="wb-mb-links visible-sm visible-xs" id="wb-glb-mn">
+            <h2><?php echo elgg_echo('wet:search');?></h2>
+            <div class="purple-bg mbm d-flex">
+                <div class="container align-self-center">
+                    <div class="app-name">
+                        <a href="<?php echo $site_url; ?>"><span><span class="bold-gc">GC</span>collab</span></a>
+                    </div>
+                    <div class="icons text-right">
+                        <a href="#mb-pnl" title="<?php echo elgg_echo('wet:search');?>" aria-controls="mb-pnl" class="overlay-lnk" role="button">
+                            <span class="glyphicon glyphicon-search"><span class="wb-inv"><?php echo elgg_echo('wet:search');?></span></span>
+                            <span class="glyphicon glyphicon-th-list"><span class="wb-inv"><?php echo elgg_echo('wet:menu');?></span></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div id="mb-pnl"></div>
+        </section>
+        <div class="container">
             <div class="row">
-                <section class="col-sm-3 col-xs-5 hidden-xs">
+                <section class="col-sm-3 col-xs-5 hidden-sm hidden-xs">
                     <div class="app-name">
                     <a href="<?php echo $site_url; ?>">
                         <span><span class="bold-gc">GC</span>collab</span>
                     </a>
                     </div>
                 </section>
-                <div class="col-md-5 col-sm-4 hidden-xs">
+                <div class="col-md-4 col-sm-4 hidden-xs">
                     <?php echo elgg_view('search/search_box', $vars); ?>
                 </div>
-                <div class="col-md-4 col-sm-5 col-xs-12">
+                <div class="col-md-5 col-sm-12 col-xs-12">
                     <?php echo elgg_view('page/elements/topbar_wrapper', $vars);?>
                 </div>
             </div>
         </div>
-
     </div>
 
 <?php } ?>
