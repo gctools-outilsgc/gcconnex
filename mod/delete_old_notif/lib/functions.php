8d8117661e4f2c1adf1ca69f98cb5fd70c50a73f
Phanoix <phanoix@gmail.com>
2019-10-02 14:45:27 -0400

    init cleanup and added delete queries

--- mod/delete_old_notif/lib/functions.php
+++ mod/delete_old_notif/lib/functions.php
@@ -3,17 +3,17 @@
 // Base on cp_notification
 function initialize_list( $cutoff_timestamp ){
 	$dbprefix = elgg_get_config('dbprefix');
-	$message_subtype = ;
+	$message_subtype = get_subtype_id( 'object', 'messages');
 	$fromId = elgg_get_metastring_id('fromId'); 
 
 	# get user guid list of old notifications and insert them all into a temp table
 	$query_table = "CREATE TABLE tmp_notify_delete_list ( `guid` bigint(20) unsigned NOT NULL, PRIMARY KEY (`guid`) )";
-	$query = "INSERT INTO tmp_notify_delete_list (guid) SELECT guid FROM {$dbprefix}entities 
-		LEFT JOIN {$db_prefix}metadata msg_fromId on e.guid = msg_fromId.entity_guid
-		LEFT JOIN {$db_prefix}metastrings msvfrom ON msg_fromId.value_id = msvfrom.id
-		LEFT JOIN {$db_prefix}entities efrom ON msvfrom.string = efrom.guid 
-		WHERE e.subtype=$message_subtype AND e.time_created < {$cutoff_timestamp}
-		AND msg_fromId.name_id='{$fromId}' AND efrom.type <> 'user'";
+	$query = "INSERT INTO tmp_notify_delete_list (guid) SELECT e.guid FROM {$dbprefix}entities e 
+LEFT JOIN {$db_prefix}metadata msg_fromId on e.guid = msg_fromId.entity_guid 
+LEFT JOIN {$db_prefix}metastrings msvfrom ON msg_fromId.value_id = msvfrom.id 
+LEFT JOIN {$db_prefix}entities efrom ON msvfrom.string = efrom.guid 
+WHERE e.subtype=$message_subtype AND e.time_created < {$cutoff_timestamp} 
+AND msg_fromId.name_id='{$fromId}' AND efrom.type <> 'user'";
 
 	try{
 		$result_table = insert_data($query_table);
@@ -26,5 +26,14 @@ function initialize_list( $cutoff_timestamp ){
 
 
 function delete_notifications(){
-	#do stuff
+	$delete_entities = "DELETE FROM {$dbprefix}entities WHERE guid IN (SELECT guid FROM tmp_notify_delete_list)";
+	$delete_objects = "DELETE FROM {$dbprefix}objects_entity WHERE guid IN (SELECT guid FROM tmp_notify_delete_list)";
+	$delete_metadata = "DELETE FROM {$dbprefix}metadata WHERE entity_guid IN (SELECT guid FROM tmp_notify_delete_list)";
+	$cleanup_tmp = "DROP TABLE tmp_notify_delete_list";
+
+
+	delete_data($delete_entities);
+	delete_data($delete_objects);
+	delete_data($delete_metadata);
+	delete_data($cleanup_tmp);
 }
