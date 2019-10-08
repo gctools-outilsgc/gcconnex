dc8db4f749bb6bc4f7b4b2752a8343cf4d7593bf
Phanoix <phanoix@gmail.com>
2019-10-08 11:17:17 -0400

    applied new header styles to completed opportunities profile section

--- mod/b_extended_profile/views/default/missions/completed-missions.php
+++ mod/b_extended_profile/views/default/missions/completed-missions.php
@@ -118,8 +118,8 @@ if($access_truth) {
 ?>
 
 <div class="panel panel-custom">
-	<div class="panel-heading profile-heading clearfix">
-		<h3 class="profile-info-head pull-left clearfix"><?php echo elgg_echo('gcconnex_profile:completed_missions'); ?></h3>
+	<div class="panel-body profile-title">
+		<h2 class="pull-left"><?php echo elgg_echo('gcconnex_profile:completed_missions'); ?></h3>
 	</div>
 	<?php echo $access_form; ?>
 	<div class="gcconnex-profile-section-wrapper panel-body gcconnex-completed-missions">
