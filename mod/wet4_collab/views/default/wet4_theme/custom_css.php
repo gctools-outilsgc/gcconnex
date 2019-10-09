df397d7385ee37f8877cb4be53cac1e3db88a3c7
Ilia <phanoix@gmail.com>
2019-10-09 09:15:45 -0400

    Profile style sgdt missed stuff (#2104)
    * This class only exists to break other css on the profile page now, removed.
    
    * Same as the connex one, it only breaks things now.
    
    * applied new header styles to completed opportunities profile section
    
    * added mid tom margin class to opt in div as in the collab side, plus some whitespace changes for readability

--- mod/wet4_collab/views/default/wet4_theme/custom_css.php
+++ mod/wet4_collab/views/default/wet4_theme/custom_css.php
@@ -2016,14 +2016,6 @@ figcaption{
     height: 0;
 }
 
-.elgg-avatar-small-wet4 > a > img {
-    width: 45px !important;
-    height: 45px !important;
-    border-radius: 50% !important;
-    background-clip: border;
-    background-size: 45px;
-}
-
 .elgg-river-message {
     margin: 12px 0;
 }
