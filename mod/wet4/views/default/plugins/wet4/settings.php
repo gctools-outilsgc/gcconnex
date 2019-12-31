862ba7adcc63d75efd4c7681fb45329d02f01d4e
Steph4104 <stephanie4104@gmail.com>
2019-12-31 08:30:07 -0500

    add timezone server option

--- mod/wet4/views/default/plugins/wet4/settings.php
+++ mod/wet4/views/default/plugins/wet4/settings.php
@@ -113,4 +113,64 @@ else {
 	);
 	echo "</div>";
 }
+
+
+
+
+echo "<div>";
+echo "Timezone server: <br />";
+function generate_timezone_list()
+{
+	static $regions = array(
+		DateTimeZone::AFRICA,
+		DateTimeZone::AMERICA,
+		DateTimeZone::ANTARCTICA,
+		DateTimeZone::ASIA,
+		DateTimeZone::ATLANTIC,
+		DateTimeZone::AUSTRALIA,
+		DateTimeZone::EUROPE,
+		DateTimeZone::INDIAN,
+		DateTimeZone::PACIFIC,
+	);
+
+	$timezones = array();
+	foreach( $regions as $region )
+	{
+		$timezones = array_merge( $timezones, DateTimeZone::listIdentifiers( $region ) );
+	}
+
+	$timezone_offsets = array();
+	foreach( $timezones as $timezone )
+	{
+		$tz = new DateTimeZone($timezone);
+		$timezone_offsets[$timezone] = $tz->getOffset(new DateTime);
+	}
+
+	// sort timezone by offset
+	asort($timezone_offsets);
+
+	$timezone_list = array();
+	foreach( $timezone_offsets as $timezone => $offset )
+	{
+		$offset_prefix = $offset < 0 ? '-' : '+';
+		$offset_formatted = gmdate( 'H:i', abs($offset) );
+
+		$pretty_offset = "UTC${offset_prefix}${offset_formatted}";
+
+		$timezone_list["$timezone"] = "(${pretty_offset}) $timezone";
+	}
+
+	return $timezone_list;
+}
+$OptionsArray = generate_timezone_list(); 
+echo elgg_view("input/select",
+	array(
+		'name' => 'params[server_timezone]',
+		'id' => 'server_timezone',
+		'value' => $vars['entity']->server_timezone ? $vars['entity']->server_timezone : '',
+		'options_values' => $OptionsArray
+	)
+);
+echo "</div>";
+
 ?>
