045b84c3b5648cc4adc80ce67334f9f6b93c02de
Ilia <phanoix@gmail.com>
2022-02-01 16:34:59 -0500

    Update start.sh
    This one is for the new default / ubuntu image

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # automated install
-php5 /var/www/html/install/cli/docker_installer.php || php /var/www/html/install/cli/docker_installer.php
+php /var/www/html/install/cli/docker_installer.php
 
 # Start server - depending on the image, one of these will work
 echo "Starting server"
