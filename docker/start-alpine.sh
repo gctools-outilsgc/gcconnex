2e8cffb3a34a405206aee650b0891fba50cb2f3b
Ilia <phanoix@gmail.com>
2022-08-09 09:39:27 -0400

    check for init in start script as well (#2441)
    * check for init in start script as well
    
    * Update start-alpine.sh
    
    * Update start-alpine.sh

--- docker/start-alpine.sh
+++ docker/start-alpine.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # automated install
-php5 /var/www/html/install/cli/docker_installer.php
+if [ $INIT ]
+then
+	echo "init detected for $INIT, running install script."
+	php5 /var/www/html/install/cli/docker_installer.php
+fi
 
 # Start server - depending on the image, one of these will work
 echo "Starting server"
