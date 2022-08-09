bef945d7e21f92aacfd2a1b7801cc40e64f82baf
Ilia <phanoix@gmail.com>
2022-08-09 09:29:53 -0400

    Update start-alpine.sh

--- docker/start-alpine.sh
+++ docker/start-alpine.sh
@@ -6,6 +6,7 @@ then
 	echo "init detected for $INIT, running install script."
 	php5 /var/www/html/install/cli/docker_installer.php
 fi
+
 # Start server - depending on the image, one of these will work
 echo "Starting server"
 $@
