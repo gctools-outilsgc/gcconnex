c96975fd556816944ada7a836691568da9a128a6
Phanoix <phanoix@gmail.com>
2023-04-12 12:02:26 -0400

    create test user

--- install/cli/docker_install_additions/e2e_test_init.php
+++ install/cli/docker_install_additions/e2e_test_init.php
@@ -7,4 +7,22 @@
 
 function e2e_init(){
     echo "do init stuff... \n";
+    create_test_user("Haibun", "Haibun", "Haibun");
+}
+
+
+function create_test_user( $username, $name, $password){
+    $user = new ElggUser();
+	$user->username = $username;
+	$user->name = $name;
+	$user->access_id = ACCESS_PUBLIC;
+	$user->setPassword($password);
+	$user->owner_guid = 0;
+	$user->container_guid = 0;
+
+	if (!$user->save()) {
+		echo "error creating user $username"
+	}
+
+    return $user;
 }
