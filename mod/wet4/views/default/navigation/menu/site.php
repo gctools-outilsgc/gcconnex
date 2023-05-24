b84ddb6fd0fb9616baaed74cbe3013a6bb2e96b2
Phanoix <phanoix@gmail.com>
2023-05-24 14:07:31 -0400

    fix extra wide site menus forcing a horizontal scroll bar on most pages

--- mod/wet4/views/default/navigation/menu/site.php
+++ mod/wet4/views/default/navigation/menu/site.php
@@ -13,7 +13,7 @@
 $default_items = elgg_extract('default', $vars['menu'], array());
 $more_items = elgg_extract('more', $vars['menu'], array());
 
-echo '<ul class="list-inline menu col-md-6" style="font-weight:500" tabindex="0" color:#333333;>';
+echo '<ul class="list-inline menu col-md-12" style="font-weight:500" tabindex="0" color:#333333;>';
 
 foreach ($default_items as $menu_item) {
 	echo elgg_view('navigation/menu/elements/item', array('item' => $menu_item, 'item-role' => 'none'));
