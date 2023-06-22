09aa31fb3e9e50d64ae415600cda0d5ee6fdf62a
Adi Makkar <adimakkar.ca@gmail.com>
2023-06-22 12:06:22 -0400

    type of opportunity requirement added

--- mod/missions_collab/views/default/forms/missions/post-mission-second-form.php
+++ mod/missions_collab/views/default/forms/missions/post-mission-second-form.php
@@ -191,8 +191,11 @@ $input_gl_group = elgg_view('input/dropdown', array(
 	</div>
 </div>
 <div class="form-group">
-	<label for='post-mission-type-dropdown-input' class="col-sm-3 text-right">
+	<label for='post-mission-type-dropdown-input' class="col-sm-3 required text-right" aria-required="true">
 		<?php echo elgg_echo('missions:opportunity_type') . ':'; ?>
+		<strong class="required" aria-required="true"> 
+			<?php echo elgg_echo('missions:required'); ?>
+		</strong> 
 	</label>
 	<div class="col-sm-3">
 		<?php echo $input_type; ?>
