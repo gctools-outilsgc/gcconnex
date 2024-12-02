b6f15ce360c94a44491750c453dcc1a304817556
Adii <73539670+AdiMakkar@users.noreply.github.com>
2024-12-02 15:24:47 -0500

    gcconnex-read-only-mode-blogs (#2688)
    * gcconnex-read-only-mode-blogs
    
    * updating the manifest

---
+++ mod/gcconnex_read_only_regular_content_types/languages/en.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+
+    'readonly:message' => 'Read Only Mode is ON, please go to GCxchange',
+        'readonly:message:1' => 'Did you know you can get the same benefits of GCconnex on GCxchange?',
+        'readonly:message:2' => 'Please visit GCxchange where you will find much more!',
+        'readonly:message:3' => '<a href="https://www.gcx-gce.gc.ca/" target="_blank">Activate your gcxchange account</a> today and check it out!',
+
+);
---
+++ mod/gcconnex_read_only_regular_content_types/start.php
@@ -0,0 +1,30 @@
+<?php
+/*
+* GC-Elgg Read Only Mode 
+*
+* Read only mode for gcconnex
+*
+* @author Adi github.com/AdiMakkar
+* @version 1.0
+*/
+
+elgg_register_event_handler('init', 'system', 'read_only_mode');
+
+function read_only_mode(){
+    
+}
+
+function decommission_message(){
+    echo "<div class='panel panel-default'>";
+    echo "<div class='panel-body'>";
+    echo "<div class='col-md-4'><img src='".$site_url."/mod/wet4/graphics/gcx_deer_in_snow.png' alt='".elgg_echo('readonly:message')."'/></div>";
+    echo "<div class='col-md-8'>";
+    echo "<div class='mrgn-lft-lg'>";
+    echo "<div class='mrgn-bttm-md h3 mrgn-tp-0'>". elgg_echo('readonly:message') ."</div>";
+    echo "<div class='mrgn-bttm-md'>". elgg_echo('readonly:message:1') ."</div>";
+    echo "<div class='mrgn-bttm-md'>". elgg_echo('readonly:message:2') ."</div>";
+    echo "<div>". elgg_echo('readonly:message:3') ."</div>";
+    echo "</div>";
+    echo "</div></div>";
+    echo "</div>";
+}
