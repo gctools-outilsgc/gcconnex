95a4ede490defc9cc3842c590ea3a4e40f95ac5f
Adi Makkar <adimakkar.ca@gmail.com>
2024-12-02 15:14:59 -0500

    gcconnex-read-only-mode-blogs

---
+++ mod/gcconnex_read_only_regular_content_types/languages/fr.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+
+    'readonly:message' => 'Le mode lecture seule est activé, veuillez vous rendre sur GCxchange',
+        'readonly:message:1' => 'Saviez-vous que vous pouvez bénéficier des mêmes avantages que GCConnex sur GCxchange?',
+        'readonly:message:2' => 'Visitez GCxchange où vous trouverez beaucoup plus!',
+        'readonly:message:3' => '<a href="https://www.gcx-gce.gc.ca/" target="_blank">Activez votre compte gcxchange</a> aujourdhui et consultez-le!',
+);
