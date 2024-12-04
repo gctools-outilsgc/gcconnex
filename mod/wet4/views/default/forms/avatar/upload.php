02af01f7332dd35341eec36c125050ea8c671963
Adi Makkar <adimakkar.ca@gmail.com>
2024-12-04 16:01:33 -0500

    avatar updated for the profile contents read only mode

--- mod/wet4/views/default/forms/avatar/upload.php
+++ mod/wet4/views/default/forms/avatar/upload.php
@@ -1,11 +1,20 @@
 <?php
-/*
-* GC-Elgg Read Only Mode 
-*
-* Read only mode for gcconnex
-*
-* @author Adi github.com/AdiMakkar
-* @version 1.0
-*/
-
-decommission_message();
+/**
+ * Avatar upload form
+ *
+ * @uses $vars['entity']
+ */
+ /*
+ * GC_MODIFICATION
+ * Description: Added accessible labels
+ * Author: GCTools Team
+ */
+?>
+<div>
+	<label for="avatar"><?php echo elgg_echo("avatar:upload"); ?></label><br />
+    <?php echo elgg_view("input/file",array('name' => 'avatar', 'id' => 'avatar','required '=> "required")); ?>
+</div>
+<div class="elgg-foot mrgn-tp-md">
+	<?php echo elgg_view('input/hidden', array('name' => 'guid', 'value' => $vars['entity']->guid)); ?>
+	<?php echo elgg_view('input/submit', array('value' => elgg_echo('upload'))); ?>
+</div>
