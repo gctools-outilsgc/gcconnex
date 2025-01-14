c645799afd8626f4b60e5df83764b6d14ae092e5
phanoix <phanoix@gmail.com>
2025-01-14 10:13:38 -0500

    fix time travel glitch

--- mod/gcconnex_read_only/languages/en.php
+++ mod/gcconnex_read_only/languages/en.php
@@ -2,9 +2,9 @@
 
 return array(
 
-    'readonly:message' => 'Read Only Mode is ON, please go to GCxchange',
-        'readonly:message:1' => 'Did you know you can get the same benefits of GCconnex on GCxchange?',
-        'readonly:message:2' => 'Please visit GCxchange where you will find much more!',
-        'readonly:message:3' => '<a href="https://www.gcx-gce.gc.ca/" target="_blank">Activate your gcxchange account</a> today and check it out!',
-
+    'readonly:message' => 'GCConnex is in Read-Only Mode',
+        'readonly:message:1' => 'You can no longer make new posts or create new content anywhere on GCConnex except for the <a href="https://www.gcconnex.gc.ca/missions/main" target="_blank">Career Marketplace</a>. You can export and save your existing content up until the decommission date on <b>April 1, 2025.</b>',
+        'readonly:message:2' => '<a href="https://www.gcconnex.gc.ca/gcconnex_is_moving" target="_blank">Learn how to export and store your GCConnex content.</a>',
+        'readonly:message:3' => 'We are encouraging all GCConnex users to move to GCXchange. Most departments have automatically created GCXchange accounts for their employees. <a href="https://www.gcxgce.sharepoint.com/SitePages/Home.aspx" target="_blank">Visit GCXchange</a> to explore the homepage.',
+        'readonly:message:4' => 'If the GCXchange link didn’t work, you might not have a GCXchange account yet. <a href="https://www.gcx-gce.gc.ca" target="_blank">Register for an account today.</a>'
 );
