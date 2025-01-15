1a151f2cd354638e87b4159e49a92f0f761a5f10
Adi Makkar <adimakkar.ca@gmail.com>
2025-01-15 14:40:14 -0500

    wip - file and photo album buttons decommissioning

---
+++ mod/gcconnex_read_only/pages/photos/owner.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Show all the albums that belong to a user or group
+ *
+ * @author Cash Costello
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2
+ */
+
+elgg_group_gatekeeper();
+$lang = get_current_language();
+$owner = elgg_get_page_owner_entity();
+
+if (!$owner) {
+	forward(REFERER);
+}
+// set up breadcrumbs
+elgg_push_breadcrumb(elgg_echo('photos'), 'photos/siteimagesall');
+elgg_push_breadcrumb(elgg_echo('tidypics:albums'), 'photos/all');
+
+	$title = elgg_echo('album:user', array(gc_explode_translation($owner->name,$lang)));
+	elgg_push_breadcrumb(gc_explode_translation($owner->name,$lang));
+
+
+
+
+$offset = (int)get_input('offset', 0);
+$limit = (int)get_input('limit', 16);
+
+$content = elgg_list_entities(array(
+	'type' => 'object',
+	'subtype' => 'album',
+	'container_guid' => $owner->getGUID(),
+	'limit' => $limit,
+	'offset' => $offset,
+	'full_view' => false,
+	'list_type' => 'gallery',
+	'list_type_toggle' => false,
+	'gallery_class' => 'tidypics-gallery',
+));
+if (!$content) {
+	$content = elgg_echo('tidypics:none');
+}
+
+if (elgg_is_logged_in()) {
+	if ($owner instanceof ElggGroup) {
+		if ($owner->isMember(elgg_get_logged_in_user_entity())) {
+			elgg_register_menu_item('title', array(
+				'name' => 'addphotos',
+				'href' => "ajax/view/photos/selectalbum/?owner_guid=" . $owner->getGUID(),
+				'text' => elgg_echo("photos:addphotos"),
+				'link_class' => 'elgg-button elgg-button-action elgg-lightbox btn btn-default btn-md'
+			));
+		}
+	} else {
+		elgg_register_menu_item('title', array(
+			'name' => 'addphotos',
+			'href' => "ajax/view/photos/selectalbum/?owner_guid=" . elgg_get_logged_in_user_guid(),
+			'text' => elgg_echo("photos:addphotos"),
+			'link_class' => 'elgg-button elgg-button-action elgg-lightbox btn btn-default btn-md'
+		));
+	}
+}
+
+
+$params = array(
+	'filter_context' => 'mine',
+	'content' => $content,
+	'title' => $title,
+	'sidebar' => elgg_view('photos/sidebar_al', array('page' => 'owner')),
+);
+
+// don't show filter if out of filter context
+if ($owner instanceof ElggGroup) {
+	$params['filter'] = false;
+}
+
+if ($owner->getGUID() != elgg_get_logged_in_user_guid()) {
+	$params['filter_context'] = '';
+}
+
+$body = elgg_view_layout('content', $params);
+
+echo elgg_view_page($title, $body);
--- mod/gcconnex_read_only/start.php
+++ mod/gcconnex_read_only/start.php
@@ -59,6 +59,300 @@ function init_ajax_block_no_edit($title, $section, $user) {
     echo '<div id="edit-' . $section . '" tabindex="-1" class="gcconnex-profile-section-wrapper panel-body gcconnex-' . $section . '">';
 }
 
+
+function tidypics_read_only($page) {
+
+	if (!isset($page[0])) {
+		return false;
+	}
+
+	elgg_require_js('tidypics/tidypics');
+	elgg_load_js('lightbox');
+	elgg_load_css('lightbox');
+	if (elgg_get_plugin_setting('slideshow', 'tidypics')) {
+		elgg_load_js('tidypics:slideshow');
+	}
+
+	$base = elgg_get_plugins_path() . 'tidypics/pages/photos';
+	$base_lists = elgg_get_plugins_path() . 'tidypics/pages/lists';
+
+    $baseReadOnly = elgg_get_plugins_path() . 'gcconnex_read_only/pages/photos';
+
+	switch ($page[0]) {
+		case "siteimagesall":
+			require "$base_lists/siteimagesall.php";
+			break;
+
+		case "siteimagesowner":
+			if (isset($page[1])) {
+				set_input('guid', $page[1]);
+			}
+			require "$base_lists/siteimagesowner.php";
+			break;
+
+		case "siteimagesfriends":
+			require "$base_lists/siteimagesfriends.php";
+			break;
+
+		case "siteimagesgroup":
+			if (isset($page[1])) {
+				set_input('guid', $page[1]);
+			}
+			require "$base_lists/siteimagesgroup.php";
+			break;
+
+		case "all": // all site albums
+		case "world":
+			require "$baseReadOnly/all.php";
+			break;
+
+		case "owned":  // albums owned by container entity
+		case "owner":
+			require "$baseReadOnly/owner.php";
+			break;
+
+		case "friends": // albums of friends
+			require "$baseReadOnly/friends.php";
+			break;
+
+		case "group": // albums of a group
+			require "$baseReadOnly/owner.php";
+			break;
+
+		case "album": // view an album individually
+			set_input('guid', $page[1]);
+			require "$base/album/view.php";
+			break;
+
+		case "new":  // create new album
+		case "add":
+			set_input('guid', $page[1]);
+			require "$base/album/add.php";
+			break;
+			
+		case "edit": //edit image or album
+			set_input('guid', $page[1]);
+			$entity = get_entity($page[1]);
+			switch ($entity->getSubtype()) {
+				case 'album':
+					require "$base/album/edit.php";
+					break;
+				case 'image':
+					require "$base/image/edit.php";
+					break;
+				case 'tidypics_batch':
+					require "$base/batch/edit.php";
+					break;
+				default:
+					return false;
+			}
+			break;
+
+		case "sort": // sort a photo album
+			set_input('guid', $page[1]);
+			require "$base/album/sort.php";
+			break;
+
+		case "image": //view an image
+		case "view":
+			set_input('guid', $page[1]);
+			require "$base/image/view.php";
+			break;
+
+		case "thumbnail": // tidypics thumbnail
+			set_input('guid', $page[1]);
+			set_input('size', elgg_extract(2, $page, 'small'));
+			require "$base/image/thumbnail.php";
+			break;
+
+		case "upload": // upload images to album
+			set_input('guid', $page[1]);
+
+			if (elgg_get_plugin_setting('uploader', 'tidypics')) {
+				$default_uploader = 'ajax';
+			} else {
+				$default_uploader = 'basic';
+			}
+
+			set_input('uploader', elgg_extract(2, $page, $default_uploader));
+			require "$base/image/upload.php";
+			break;
+
+		case "download": // download an image
+			set_input('guid', $page[1]);
+			set_input('disposition', elgg_extract(2, $page, 'attachment'));
+			include "$base/image/download.php";
+			break;
+
+		case "tagged": // all photos tagged with user
+			if (isset($page[1])) {
+				set_input('guid', $page[1]);
+			}
+			require "$base/tagged.php";
+			break;
+
+		case "mostviewed": // images with the most views
+			if (isset($page[1])) {
+				set_input('username', $page[1]);
+			}
+			require "$base_lists/mostviewedimages.php";
+			break;
+
+		case "mostviewedtoday":
+			if (isset($page[1])) {
+				set_input('username', $page[1]);
+			}
+			require "$base_lists/mostviewedimagestoday.php";
+			break;
+
+		case "mostviewedthismonth":
+			if (isset($page[1])) {
+				set_input('username', $page[1]);
+			}
+			require "$base_lists/mostviewedimagesthismonth.php";
+			break;
+
+		case "mostviewedlastmonth":
+			if (isset($page[1])) {
+				set_input('username', $page[1]);
+			}
+			require "$base_lists/mostviewedimageslastmonth.php";
+			break;
+
+		case "mostviewedthisyear":
+			if (isset($page[1])) {
+				set_input('username', $page[1]);
+			}
+			require "$base_lists/mostviewedimagesthisyear.php";
+			break;
+
+		case "mostcommented":
+			if (isset($page[1])) {
+				set_input('username', $page[1]);
+			}
+			require "$base_lists/mostcommentedimages.php";
+			break;
+
+		case "mostcommentedtoday":
+			if (isset($page[1])) {
+				set_input('username', $page[1]);
+			}
+			require "$base_lists/mostcommentedimagestoday.php";
+			break;
+
+		case "mostcommentedthismonth":
+			if (isset($page[1])) {
+				set_input('username', $page[1]);
+			}
+			require "$base_lists/mostcommentedimagesthismonth.php";
+			break;
+
+		case "mostcommentedlastmonth":
+			if (isset($page[1])) {
+				set_input('username', $page[1]);
+			}
+			require "$base_lists/mostcommentedimageslastmonth.php";
+			break;
+
+		case "mostcommentedthisyear":
+			if (isset($page[1])) {
+				set_input('username', $page[1]);
+			}
+			require "$base_lists/mostcommentedimagesthisyear.php";
+			break;
+
+		case "recentlyviewed":
+			require "$base_lists/recentlyviewed.php";
+			break;
+
+		case "recentlycommented":
+			require "$base_lists/recentlycommented.php";
+			break;
+
+		case "recentvotes":
+			if(elgg_is_active_plugin('elggx_fivestar')) {
+				require "$base_lists/recentvotes.php";
+				break;
+			} else {
+				return false;
+			}
+
+		case "highestrated":
+			if(elgg_is_active_plugin('elggx_fivestar')) {
+				require "$base_lists/highestrated.php";
+				break;
+			} else {
+				return false;
+			}
+
+		case "highestvotecount":
+			if(elgg_is_active_plugin('elggx_fivestar')) {
+				require "$base_lists/highestvotecount.php";
+				break;
+			} else {
+				return false;
+			}
+
+		default:
+			return false;
+	}
+
+	return true;
+}
+
+function file_read_only_handler($page) {
+
+	if (!isset($page[0])) {
+		$page[0] = 'all';
+	}
+
+	$file_dir = elgg_get_plugins_path() . 'file/pages/file';
+	$file_dir_readonly = elgg_get_plugins_path() . 'gcconnex_read_only/pages/file';
+
+	$page_type = $page[0];
+	switch ($page_type) {
+		case 'owner':
+			file_register_toggle();
+			include "$file_dir_readonly/owner.php";
+			break;
+		case 'friends':
+			file_register_toggle();
+			include "$file_dir_readonly/friends.php";
+			break;
+		case 'view':
+			elgg_push_context('view_file');
+			set_input('guid', $page[1]);
+			include "$file_dir/view.php";
+			break;
+		case 'add':
+			include "$file_dir/upload.php";
+			break;
+		case 'edit':
+			set_input('guid', $page[1]);
+			include "$file_dir/edit.php";
+			break;
+		case 'search':
+			file_register_toggle();
+			include "$file_dir/search.php";
+			break;
+		case 'group':
+			file_register_toggle();
+			include "$file_dir/owner.php";
+			break;
+		case 'all':
+			file_register_toggle();
+			include "$file_dir_readonly/world.php";
+			break;
+		case 'download':
+			set_input('guid', $page[1]);
+			include "$file_dir/download.php";
+			break;
+		default:
+			return false;
+	}
+	return true;
+}
+
 function decommission_message() {
 
     echo "<div class='panel panel-default'>
