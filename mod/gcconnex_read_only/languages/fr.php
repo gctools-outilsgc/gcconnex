b16d871703a125bda132a694c1dbe80801b07864
Adi Makkar <adimakkar.ca@gmail.com>
2025-01-22 10:08:13 -0500

    this is a wip comment to generate a test instance

--- mod/gcconnex_read_only/languages/fr.php
+++ mod/gcconnex_read_only/languages/fr.php
@@ -7,4 +7,5 @@ return array(
         'readonly:message:2' => '<a href="https://gcconnex.gc.ca/gcconnex_demenage" target="_blank">Directives sur l’exportation et le stockage de votre contenu GCConnex.</a>',
         'readonly:message:3' => 'Nous invitons les utilisateurs de GCConnex à migrer vers GCÉchange. La plupart des ministères ont automatiquement créé des comptes GCÉchange pour leurs employés. <a href="https://gcxgce.sharepoint.com/SitePages/fr/Home.aspx" target="_blank">Cliquez ici</a> pour découvrir la page d’accueil de GCÉchange.',
         'readonly:message:4' => 'Si le lien ci-dessus ne fonctionne pas, c’est peut-être parce que vous n’avez pas encore de compte. <a href="https://www.gcx-gce.gc.ca" target="_blank">Inscrivez-vous à GCÉchange dès aujourd’hui.</a>'
+        
 );
