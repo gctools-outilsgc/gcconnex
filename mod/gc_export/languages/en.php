21bec3188ddac31aaa325339b93a49c9cdfc7eac
phanoix <phanoix@gmail.com>
2025-02-25 09:57:17 -0500

    added email list link text

--- mod/gc_export/languages/en.php
+++ mod/gc_export/languages/en.php
@@ -2,6 +2,7 @@
 
 	$english = array(
 		'decommission:discussion:export' => 'Export the Discussion',
+		'decommission:email_list' => 'Members Email List'
 	);
 
 	add_translation("en", $english);
--- mod/gc_export/languages/fr.php
+++ mod/gc_export/languages/fr.php
@@ -2,6 +2,7 @@
 
 	$french = array(
 		'decommission:discussion:export' => 'Exporter la discussion',
+		'decommission:email_list' => 'Liste de courriels des membres'
 	);
 
 	add_translation("fr", $french);
