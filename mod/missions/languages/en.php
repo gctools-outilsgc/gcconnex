2831b9dc276624bec145fa8e1a5602dafa39cc41
Adii <73539670+AdiMakkar@users.noreply.github.com>
2025-06-16 09:18:26 -0400

    login and CM banner comms updated (#2728)
    * login and CM banner comms updated
    
    * irregularities in comms updated
    
    * updated the comms based of language issue

--- mod/missions/languages/en.php
+++ mod/missions/languages/en.php
@@ -674,9 +674,10 @@ Should you no longer be available to participate in this opportunity, please wit
 "missions:settings:analytics_on" =>  "Display analytics tab?",
 "missions:cannot_cancel_mission_with_participants" => "Please remove all active participants and all applicants to whom you have sent an offer before deactivating this opportunity.",
 "missions:required" =>    "(required)",
-"missions:placeholder_a" =>   "<b>The Career Marketplace is moving!</b> The Career Marketplace is moving to GCXchange, the new government-wide collaboration platform. 
-The Career Marketplace will continue to be available on GCConnex until it launches on GCXchange by May 1, 2025. 
-There will be no interruptions! You can continue to explore career development opportunities, connect with mentors, and browse postings from across the GC.
+"missions:placeholder_a" =>   "<b>The Career Marketplace is moving to GCXchange later this fiscal year!</b> The Career Marketplace is moving to GCXchange, the new government-wide collaboration platform. 
+The Career Marketplace will continue to be available on GCConnex until it launches on GCXchange <b>later this fiscal year</b>. 
+Once the new Career Marketplace launches on GCXchange, this page will redirect you there. 
+In the meantime, you can continue to explore career development opportunities, connect with mentors, and browse postings from across the GC.
 <br>
 <br>
 Thank you for being part of this exciting transition. If you have questions, reach out to us at <b><a href='support-soutien@gcx-gce.gc.ca'>support-soutien@gcx-gce.gc.ca</a></b>",
