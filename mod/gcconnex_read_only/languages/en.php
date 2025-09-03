e8e0e1761aa934834ae39993584a6e1c7898447d
Ilia <phanoix@gmail.com>
2025-09-03 13:24:56 -0400

    Update en.php

--- mod/gcconnex_read_only/languages/en.php
+++ mod/gcconnex_read_only/languages/en.php
@@ -3,7 +3,7 @@
 return array(
 
     'readonly:message' => 'GCConnex will be decommissioned on October 14, 2025.',
-        'readonly:message:1' => 'The <a href="https://gcconnex.gc.ca/missions/main" target="_blank">Career Marketplace</a> becomes read-only when the new Career Marketplace launches on GCXchange on <b>October 1, 2025</b>.  Make sure to <b>save or export</b> your existing content before GCConnex is fully decommissioned on <b>October 14, 2025</b>.',
+        'readonly:message:1' => 'The Career Marketplace becomes read-only when the new Career Marketplace launches on GCXchange on <b>October 1, 2025</b>.  Make sure to <b>save or export</b> your existing content before GCConnex is fully decommissioned on <b>October 14, 2025</b>.',
         'readonly:message:2' => '<a href="https://gcconnex.gc.ca/gcconnex_is_moving" target="_blank">Learn how to export and store your GCConnex content.</a>',
         'readonly:message:3' => 'We are encouraging all GCConnex users to move to GCXchange. Most departments have automatically created GCXchange accounts for their employees. <a href="https://gcxgce.sharepoint.com/SitePages/Home.aspx" target="_blank">Visit GCXchange</a> to explore the homepage.',
         'readonly:message:4' => 'If the GCXchange link didn’t work, you might not have a GCXchange account yet. <a href="https://www.gcx-gce.gc.ca" target="_blank">Register for an account today.</a>'
