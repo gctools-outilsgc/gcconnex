064f84241c1526b76bdab447c5d76eb794ec5403
Ilia <phanoix@gmail.com>
2025-09-26 14:05:40 -0400

    add placeholder CM page message override

--- mod/gcconnex_read_only_CM/languages/en.php
+++ mod/gcconnex_read_only_CM/languages/en.php
@@ -6,5 +6,12 @@ return array(
         'readonly:message:1' => 'You can no longer make new posts or create new content anywhere on GCConnex except for the <a href="https://gcconnex.gc.ca/missions/main" target="_blank">Career Marketplace</a>. You can export and save your existing content up until the <b>decommission later this fiscal year.</b>',
         'readonly:message:2' => '<a href="https://gcconnex.gc.ca/gcconnex_is_moving" target="_blank">Learn how to export and store your GCConnex content.</a>',
         'readonly:message:3' => 'We are encouraging all GCConnex users to move to GCXchange. Most departments have automatically created GCXchange accounts for their employees. <a href="https://gcxgce.sharepoint.com/SitePages/Home.aspx" target="_blank">Visit GCXchange</a> to explore the homepage.',
-        'readonly:message:4' => 'If the GCXchange link didn’t work, you might not have a GCXchange account yet. <a href="https://www.gcx-gce.gc.ca" target="_blank">Register for an account today.</a>'
+        'readonly:message:4' => 'If the GCXchange link didn’t work, you might not have a GCXchange account yet. <a href="https://www.gcx-gce.gc.ca" target="_blank">Register for an account today.</a>',
+        "missions:placeholder_a" =>   "The Career Marketplace has moved to GCXchange! This page is read-only. 
+Visit gcx
+sove or export
+<br>
+<br>
+Thank you for being part of this exciting transition. If you have questions, reach out to us at <b><a href='support-soutien@gcx-gce.gc.ca'>support-soutien@gcx-gce.gc.ca</a></b>",
+
 );
