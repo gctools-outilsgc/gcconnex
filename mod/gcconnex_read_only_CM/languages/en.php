7b427d951983a5407cc0861315a77d9a68ced85d
Ilia <phanoix@gmail.com>
2025-09-29 12:58:15 -0400

    update to full read-only gcconnex messaging (#2733)
    * add placeholder CM page message override
    
    * Update English messaging
    
    * en formatting tweaks
    
    * Update en.php
    
    * Update fr.php
    
    * Update fr.php

--- mod/gcconnex_read_only_CM/languages/en.php
+++ mod/gcconnex_read_only_CM/languages/en.php
@@ -2,9 +2,15 @@
 
 return array(
 
-    'readonly:message' => 'GCConnex is in Read-Only Mode',
-        'readonly:message:1' => 'You can no longer make new posts or create new content anywhere on GCConnex except for the <a href="https://gcconnex.gc.ca/missions/main" target="_blank">Career Marketplace</a>. You can export and save your existing content up until the <b>decommission later this fiscal year.</b>',
-        'readonly:message:2' => '<a href="https://gcconnex.gc.ca/gcconnex_is_moving" target="_blank">Learn how to export and store your GCConnex content.</a>',
-        'readonly:message:3' => 'We are encouraging all GCConnex users to move to GCXchange. Most departments have automatically created GCXchange accounts for their employees. <a href="https://gcxgce.sharepoint.com/SitePages/Home.aspx" target="_blank">Visit GCXchange</a> to explore the homepage.',
-        'readonly:message:4' => 'If the GCXchange link didn’t work, you might not have a GCXchange account yet. <a href="https://www.gcx-gce.gc.ca" target="_blank">Register for an account today.</a>'
+    'readonly:message' => 'GCConnex will be decommissioned on October 14, 2025.',
+        'readonly:message:1' => 'Make sure to <b>save or export</b> your existing content before GCConnex is fully decommissioned on <b>October 14, 2025</b>.',
+        'readonly:message:2' => '<a href="https://gcxgce.sharepoint.com/sites/CareerMarketplace/SitePages/Find-an-Opportunity---Career-Marketplace.aspx">Explore the new Career Marketplace on GCXchange.</a>  <br /><br /> <a href="https://gcconnex.gc.ca/gcconnex_is_moving" target="_blank">Learn how to export and store your GCConnex content.</a>',
+        'readonly:message:3' => 'GCXchange is replacing GCConnex. Most departments have automatically created GCXchange accounts for their employees. <a href="https://gcxgce.sharepoint.com/SitePages/Home.aspx" target="_blank">Visit GCXchange</a> to explore the homepage.',
+        'readonly:message:4' => 'If the GCXchange link doesn’t work, you might not have a GCXchange account yet. <a href="https://www.gcx-gce.gc.ca" target="_blank">Register for an account today.</a>',
+        "missions:placeholder_a" =>   "The Career Marketplace has moved to GCXchange! This page is now read-only. <br />
+<a href='https://gcxgce.sharepoint.com/sites/CareerMarketplace/SitePages/Find-an-Opportunity---Career-Marketplace.aspx'>Visit the new Career Marketplace on GCXchange</a>. <br />
+Make sure to <b>save or export</b> any information you want to keep before GCConnex is fully decommissioned on <b>October 14, 2025</b>.<br /> <a href='https://gcconnex.gc.ca/gcconnex_is_moving'>Learn how to export your GCConnex content</a>. <br />
+<br>
+<br>
+Thank you for being part of this exciting transition. If you have questions, reach out to us at <b><a href='support-soutien@gcx-gce.gc.ca'>support-soutien@gcx-gce.gc.ca</a></b>",
 );
--- mod/gcconnex_read_only_CM/languages/fr.php
+++ mod/gcconnex_read_only_CM/languages/fr.php
@@ -2,9 +2,15 @@
 
 return array(
 
-    'readonly:message' => 'GCConnex est en mode lecture seule',
-        'readonly:message:1' => 'Désormais, vous ne pouvez plus publier de messages ni créer de contenu dans GCConnex, à l’exception du <a href="https://gcconnex.gc.ca/missions/main" target="_blank">Carrefour de carrière</a>. Vous pouvez exporter et sauvegarder votre contenu existant jusqu’à la <b>date de mise hors service, qui a été reportée à une date ultérieure au cours du présent exercice.</b>',
-        'readonly:message:2' => '<a href="https://gcconnex.gc.ca/gcconnex_demenage" target="_blank">Directives sur l’exportation et le stockage de votre contenu GCConnex.</a>',
-        'readonly:message:3' => 'Nous invitons les utilisateurs de GCConnex à migrer vers GCÉchange. La plupart des ministères ont automatiquement créé des comptes GCÉchange pour leurs employés. <a href="https://gcxgce.sharepoint.com/SitePages/fr/Home.aspx" target="_blank">Cliquez ici</a> pour découvrir la page d’accueil de GCÉchange.',
-        'readonly:message:4' => 'Si le lien ci-dessus ne fonctionne pas, c’est peut-être parce que vous n’avez pas encore de compte. <a href="https://www.gcx-gce.gc.ca" target="_blank">Inscrivez-vous à GCÉchange dès aujourd’hui.</a>'
+    'readonly:message' => 'GCConnex sera hors service à compter du 14 octobre 2025.',
+        'readonly:message:1' => 'Veillez <b>à sauvegarder ou à exporter</b> votre contenu avant le <b>14 octobre 2025</b>, date où GCConnex sera complètement hors service.',
+        'readonly:message:2' => '<a href="https://gcxgce.sharepoint.com/sites/CareerMarketplace/SitePages/fr/Find-an-Opportunity---Career-Marketplace.aspx">Cliquez ici pour consulter le Marché de Carrière sur GCÉchange.</a> <br /><br /> <a href="https://gcconnex.gc.ca/gcconnex_demenage" target="_blank">Cliquez ici pour savoir comment exporter le contenu de GCConnex.</a>',
+        'readonly:message:3' => 'GCÉchange remplace GCConnex. La plupart des ministères ont déjà créé des comptes GCÉchange pour les membres de leur personnel. <a href="https://gcxgce.sharepoint.com/SitePages/fr/Home.aspx">Cliquez ici pour découvrir la page d’accueil de GCÉchange</a>.',
+        'readonly:message:4' => 'Si le lien ci-dessus ne fonctionne pas, c’est peut-être parce que vous n’avez pas encore de compte. <a href="https://www.gcx-gce.gc.ca" target="_blank">Inscrivez-vous à GCÉchange dès aujourd’hui.</a>',
+        "missions:placeholder_a" =>   "Le Marché de Carrière est désormais sur GCÉchange! La présente page est en mode lecture seule. <br />
+<a href='https://gcxgce.sharepoint.com/sites/CareerMarketplace/SitePages/fr/Find-an-Opportunity---Career-Marketplace.aspx'>Cliquez ici pour consulter le nouveau Marché de Carrière sur GCÉchange.</a>. <br />
+Veillez <b>à sauvegarder ou à exporter</b> les renseignements auxquels vous tenez avant le <b>14 octobre 2025</b>, date où GCConnex sera complètement hors service. <br /> <a href='https://gcconnex.gc.ca/gcconnex_is_moving'>Cliquez ici pour savoir comment exporter le contenu de GCConnex.</a> <br />
+<br>
+<br>
+Merci de prendre part à cette transition prometteuse. Pour toute question, écrivez-nous à <b><a href='support-soutien@gcx-gce.gc.ca'>support-soutien@gcx-gce.gc.ca</a></b>",
 );
