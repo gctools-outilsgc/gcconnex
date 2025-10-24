d63a82ca110f252f03ce50ef59e273f7fc9d7174
Ilia <phanoix@gmail.com>
2025-10-24 13:50:37 -0400

    fix gcaccount logout url redirect (#2734)
    /action/ is an elgg thing, it's just /logout in gcaccount

--- mod/pleio/actions/logout.php
+++ mod/pleio/actions/logout.php
@@ -4,7 +4,7 @@ $auth_url = elgg_get_plugin_setting('auth_url', 'pleio');
 $result = logout();
 
 if ($result) {
-    forward($auth_url . "action/logout");
+    forward($auth_url . "logout");
 } else {
     register_error(elgg_echo('logouterror'));
 }
